09576869ef9b3f8ffaf0bef0569a853c2dc57995
jungmini <mins0428@gmail.com>
2021-04-28 09:54:41 +0900

    Update _WebScrap.py

--- NtosMini/_WebScrap.py
+++ NtosMini/_WebScrap.py
@@ -24,31 +24,28 @@ else :
 	from selenium import webdriver
 	from selenium.webdriver.common.alert import Alert
 
-	if WebType == "Chrome" :
-#		chrome_options = webdriver.ChromeOptions()
-#		chrome_options.add_argument('headless')
-#		chrome_options.add_argument('--disable-gpu')
-#		chrome_options.add_argument('lang=ko_KR')
-#		chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36")
-#		driver = webdriver.Chrome("C:/xampp/htdocs/_Scrap/chromedriver", chrome_options=chrome_options)
+	if WebType == "WinChrome" :
 		driver = webdriver.Chrome("C:/xampp/htdocs/NtosMini/chromedriver.exe")
 	else :	# 기본 : Firefox
 		driver = webdriver.Firefox("C:/xampp/htdocs/NtosMini/geckodriver.exe")
 
 	if Referer != "not" :
 		driver.get(Referer);
-		time.sleep(2)
+		time.sleep(random.randint(1, 3))
+
+	#종료
+	def DriverQuit():
+		driver.quit()
+		print('')
+		exit()
+
+	DriverJob = threading.Timer(300, DriverQuit)
+	DriverJob.start()
 
 	driver.get(SiteUrl);
 	time.sleep(3)
 
-	#경고창
-	try :
-		result = driver.switch_to_alert()
-#		result.accept()
-		result.dismiss()
-	except :
-		pass
+	DriverJob.cancel()
 
 
 	page_html = driver.page_source
