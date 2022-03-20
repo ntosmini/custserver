96e221ad800b3efc194956987d18a3bd88f7a708
jungmini <mins0428@gmail.com>
2022-03-21 08:32:14 +0900

    Update _WebScrap_ub.php

--- NtosMini/_WebScrap_ub.php
+++ NtosMini/_WebScrap_ub.php
@@ -1,7 +1,9 @@
 <?php
 set_time_limit(0);
 header("Content-Type: text/html; charset=UTF-8");
-
+/*
+2022-03-21
+*/
 
 $SiteUrl = (empty($_POST['SiteUrl']))?"not":$_POST['SiteUrl'];
 $WebType = (empty($_POST['WebType']))?"Chrome":$_POST['WebType'];	// "Chrome" or "Firefox" or "curl"
