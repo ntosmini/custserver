c39096c02891f868052a8aa78d7a875ffe5aea69
jungmini <mins0428@gmail.com>
2022-03-22 07:39:22 +0900

    Update test_php.php

--- NtosMini/test_php.php
+++ NtosMini/test_php.php
@@ -10,10 +10,7 @@ curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-
-$Agent = ($Agent == "not")?"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36":$Agent;
-curl_setopt($ch, CURLOPT_USERAGENT, $Agent);
-
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36");
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120); //
 curl_setopt($ch, CURLOPT_TIMEOUT, 300); //
 $PageHtml=curl_exec($ch);
