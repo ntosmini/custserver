a7abbdff92471f7900477ae1630becb4229f6054
jungmini <mins0428@gmail.com>
2022-09-16 07:51:15 +0900

    Update category_multi.py

--- __NtosSc__/ali/category_multi.py
+++ __NtosSc__/ali/category_multi.py
@@ -62,111 +62,107 @@ def multiSelenium(process):
 
 	driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=chrome_options)
 
-	#종료
-	def DriverQuit():
+
+	try :
+		driver.get(SiteUrl)
+		driver.implicitly_wait(10)
+		driver.refresh()
+		driver.implicitly_wait(5)
+		time.sleep(random.randint(3, 5))
+
+
+		SCROLL_PAUSE_SEC = 1
+		# 스크롤 높이 가져옴
+		last_height = driver.execute_script("return document.body.scrollHeight")
+
+		while True:
+			# 끝까지 스크롤 다운
+			driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+			# 1초 대기
+			time.sleep(SCROLL_PAUSE_SEC)
+
+			# 스크롤 다운 후 스크롤 높이 다시 가져옴
+			new_height = driver.execute_script("return document.body.scrollHeight")
+			if new_height == last_height:
+				break
+			last_height = new_height
+
+
+		time.sleep(3)
+
+		page_html = driver.page_source
+		if page_html :
+			ItemList = []
+			
+
+			try :
+				ScriptMatched = re.search('\{"mods".*\}', page_html)
+				ScriptListData = ScriptMatched.group()
+
+				ScriptListDataArr = json.loads(ScriptListData)
+				for Slist in ScriptListDataArr['mods']['itemList']['content'] :
+					Code1 = str(Slist['productId'])
+					Url = '/item/'+Code1+".html"
+					if len(Code1) > CodeLen :
+						ItemList.append(Code1)
+						
+			except :
+				pass
+
+			try :
+				ItemContentBox = driver.find_element_by_class_name("product-container")
+				ATagItems = ItemContentBox.find_elements_by_tag_name('a')
+
+				ItemClassName = ''
+				for ATI in ATagItems :
+					href = ATI.get_attribute('href')
+					if re.search("aliexpress.com/item/\d+", str(href)) :
+						ItemClassName = ATI.get_attribute('class')
+						break
+				if ItemClassName :
+					for ATI2 in ATagItems :
+						href = ATI2.get_attribute('href')
+						class_ = ATI2.get_attribute('class')
+						if class_ == ItemClassName and re.search("aliexpress.com/item/\d+", str(href)) :
+							href_result = re.sub(r'(\.html.*)$', '.html', str(href))
+							Code2 = re.search("\d+", href_result).group()
+							if len(str(Code2)) > CodeLen :
+								ItemList.append(str(Code2))
+
+			except :
+				ItemContentBox = ""
+
+			ItemListSet = list(set(ItemList))
+			Result = ""
+			if len(ItemListSet) > 0 :
+				Result = "success"
+			else :
+				if ItemContentBox :
+					Result = "notitem"
+				else :
+					Result = "error"
+
+
+			data = {'NotsKey':NotsKey, 'CustId':CustId, 'SclId':SclId, 'Result':Result, 'ItemList': ItemListSet, 'log_id': log_id }
+			headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+			Result_ = ""
+			try :
+				Result__ = requests.post(NtosServer, data=json.dumps(data), headers=headers)
+				Result_ = Result__.text
+			except :
+				Result_ = "error"
+		else :
+			Result_ = "error"
+
+		print(Result_)
 		driver.close()
 		driver.quit()
-
-
-	DriverJob = threading.Timer(120, DriverQuit)
-	DriverJob.start()
-
-	driver.get(SiteUrl)
-	driver.implicitly_wait(10)
-	driver.refresh()
-	driver.implicitly_wait(5)
-	time.sleep(random.randint(3, 5))
-
-	DriverJob.cancel()
-
-	SCROLL_PAUSE_SEC = 1
-	# 스크롤 높이 가져옴
-	last_height = driver.execute_script("return document.body.scrollHeight")
-
-	while True:
-		# 끝까지 스크롤 다운
-		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-		# 1초 대기
-		time.sleep(SCROLL_PAUSE_SEC)
-
-		# 스크롤 다운 후 스크롤 높이 다시 가져옴
-		new_height = driver.execute_script("return document.body.scrollHeight")
-		if new_height == last_height:
-			break
-		last_height = new_height
-
-
-	time.sleep(3)
-
-	page_html = driver.page_source
-	if page_html :
-		ItemList = []
-		
-
-		try :
-			ScriptMatched = re.search('\{"mods".*\}', page_html)
-			ScriptListData = ScriptMatched.group()
-
-			ScriptListDataArr = json.loads(ScriptListData)
-			for Slist in ScriptListDataArr['mods']['itemList']['content'] :
-				Code1 = str(Slist['productId'])
-				Url = '/item/'+Code1+".html"
-				if len(Code1) > CodeLen :
-					ItemList.append(Code1)
-					
-		except :
-			pass
-
-		try :
-			ItemContentBox = driver.find_element_by_class_name("product-container")
-			ATagItems = ItemContentBox.find_elements_by_tag_name('a')
-
-			ItemClassName = ''
-			for ATI in ATagItems :
-				href = ATI.get_attribute('href')
-				if re.search("aliexpress.com/item/\d+", str(href)) :
-					ItemClassName = ATI.get_attribute('class')
-					break
-			if ItemClassName :
-				for ATI2 in ATagItems :
-					href = ATI2.get_attribute('href')
-					class_ = ATI2.get_attribute('class')
-					if class_ == ItemClassName and re.search("aliexpress.com/item/\d+", str(href)) :
-						href_result = re.sub(r'(\.html.*)$', '.html', str(href))
-						Code2 = re.search("\d+", href_result).group()
-						if len(str(Code2)) > CodeLen :
-							ItemList.append(str(Code2))
-
-		except :
-			ItemContentBox = ""
-
-		ItemListSet = list(set(ItemList))
-		Result = ""
-		if len(ItemListSet) > 0 :
-			Result = "success"
-		else :
-			if ItemContentBox :
-				Result = "notitem"
-			else :
-				Result = "error"
-
-
-		data = {'NotsKey':NotsKey, 'CustId':CustId, 'SclId':SclId, 'Result':Result, 'ItemList': ItemListSet, 'log_id': log_id }
-		headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-
-		Result_ = ""
-		try :
-			Result__ = requests.post(NtosServer, data=json.dumps(data), headers=headers)
-			Result_ = Result__.text
-		except :
-			Result_ = "error"
-	else :
-		Result_ = "error"
-
-	print(Result_)
-	driver.close()
-	driver.quit()
+	except :
+		print("except")
+		driver.close()
+		driver.quit()
 
 
 if __name__ == '__main__':
