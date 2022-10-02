c94aecaea55a981247ed0a91c276a1343c203b13
jungmini <mins0428@gmail.com>
2022-10-02 11:29:37 +0900

    Add files via upload

---
+++ __NtosSc__/ali/item_multi.php
@@ -0,0 +1,30 @@
+<?php
+set_time_limit(0);
+header("Content-Type: text/html; charset=UTF-8");
+/*
+알리 카테고리 - 멀티
+*/
+
+$RunData = array();
+$RunData['NtosServer'] = (empty($_POST['NtosServer']))?"":$_POST['NtosServer'];
+$RunData['SlId_SiteUrl'] = (empty($_POST['SlId_SiteUrl']))?"":$_POST['SlId_SiteUrl'];
+$RunData['Agent'] = (empty($_POST['Agent']))?"":$_POST['Agent'];
+$RunData['CodeLen'] = (empty($_POST['CodeLen']))?"15":$_POST['CodeLen'];
+$RunData['NotsKey'] = (empty($_POST['NotsKey']))?"":$_POST['NotsKey'];
+$RunData['CustId'] = (empty($_POST['CustId']))?"":$_POST['CustId'];
+
+
+if(empty($RunData['NtosServer']) || empty($RunData['SlId_SiteUrl']) || empty($RunData['CustId']) || empty($RunData['NotsKey'])){
+	echo 'error';
+	exit;
+}	//end if
+
+
+
+$MConfigData = escapeshellarg(json_encode($RunData));
+
+
+exec("python3 /home/ntosmini/public_html/__NtosSc__/ali/item_multi.py {$MConfigData}", $ResultArr);
+$PageHtml = implode("\n", $ResultArr);
+
+echo $PageHtml;
---
+++ __NtosSc__/ali/item_multi.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*- 
+# 알리 카테고리 멀티
+
+import time
+import sys
+import codecs
+import random
+#pip install beautifulsoup4     <=> pip install bs4
+import threading
+import json
+import re
+import io
+import os
+import multiprocessing
+import requests
+
+#한글깨짐
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+
+
+try :
+	os.system("killall -o 3m chrome")
+	os.system("killall -o 3m chromedriver")
+except :
+	pass
+
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+
+process_list = []
+
+
+MConfigData = sys.argv[1]
+MConfig = json.loads(MConfigData)
+
+
+NtosServer = MConfig['NtosServer']
+process_list = MConfig['SlId_SiteUrl']
+Agent = MConfig['Agent']
+CodeLen = int(MConfig['CodeLen'])
+NotsKey = MConfig['NotsKey']
+CustId = MConfig['CustId']
+
+
+start_time = time.time()
+def multiSelenium(process):
+	
+	(SlId, SiteUrl, log_id) = process.split("|@|")
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument("-disable-notifications")
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chrome_options.add_argument("--window-size=1920x1080")
+	if Agent == "" :
+		chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
+	else :
+		chrome_options.add_argument("user-agent="+ Agent)
+
+
+	driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=chrome_options)
+
+
+	try :
+		driver.get(SiteUrl)
+		driver.implicitly_wait(10)
+		time.sleep(random.randint(3, 5))
+
+
+		page_html = driver.page_source
+		if page_html :
+
+			data = {'NtosServer':str(NtosServer), 'NotsKey':NotsKey, 'CustId':CustId, 'SlId':SlId, 'PageHtml':page_html }
+			headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+			Result_ = ""
+			try :
+				Result__ = requests.post(NtosServer, data=json.dumps(data), headers=headers)
+				Result_ = Result__.text
+			except :
+				Result_ = "error"
+		else :
+			Result_ = "error"
+
+		print(Result_)
+		driver.close()
+		driver.quit()
+	except :
+		print("except")
+		driver.close()
+		driver.quit()
+
+
+if __name__ == '__main__':
+	pool = multiprocessing.Pool(processes=len(process_list))
+	pool.map(multiSelenium, process_list)
+	#print("\n\n--- %s seconds ---" % (time.time() - start_time))
+	pool.close()
+	pool.join()
+	sys.exit()
+
