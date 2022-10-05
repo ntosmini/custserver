3fe9678e843ab6eeb868cd0fbdb9e5dbf359a96e
jungmini <mins0428@gmail.com>
2022-10-05 15:20:31 +0900

    Update item_multi.py

--- __NtosSc__/ali/item_multi.py
+++ __NtosSc__/ali/item_multi.py
@@ -27,7 +27,12 @@ except :
 	pass
 
 from selenium import webdriver
-from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+
+#pip3 install webdriver_manager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 process_list = []
 
@@ -37,78 +42,97 @@ MConfig = json.loads(MConfigData)
 
 
 NtosServer = MConfig['NtosServer']
-process_list = MConfig['SlId_SiteUrl']
-Agent = MConfig['Agent']
-CodeLen = int(MConfig['CodeLen'])
+process_list = MConfig['SlId_SiteUrl_Proxy']
 NotsKey = MConfig['NotsKey']
 CustId = MConfig['CustId']
 
-
 start_time = time.time()
-def multiSelenium(process):
-	
-	(SlId, SiteUrl, log_id) = process.split("|@|")
-	chrome_options = webdriver.ChromeOptions()
-	chrome_options.add_argument("-disable-notifications")
+
+
+def chromeWebdriver(Proxy):
+	chrome_service = ChromeService(executable_path=ChromeDriverManager().install())
+	chrome_options = Options()
+	chrome_options.add_experimental_option('detach', True)
+	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+	"""
+	prefs = {
+	  "translate_whitelists": {"your native language":"ko"},
+	  "translate":{"enabled":"true"}
+	}
+	chrome_options.add_experimental_option('prefs', prefs)
+	"""
+
 	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--lang=ko_KR')
 	chrome_options.add_argument('--no-sandbox')
-	chrome_options.add_argument('--disable-dev-shm-usage')
-	chrome_options.add_argument("--window-size=1920x1080")
-	if Agent == "" :
-		chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
-	else :
-		chrome_options.add_argument("user-agent="+ Agent)
 
 
-	driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=chrome_options)
+	chrome_options.add_argument("window-size=1920,1080")
 
+	if Proxy :	# IP:PORT or HOST:PORT
+		chrome_options.add_argument('--proxy-server=%s' % Proxy)
+
+	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+	return driver
+
+
+def multiSelenium(process):
+	(SlId, SiteUrl, Proxy, log_id) = process.split("|@|")
+
+	driver = chromeWebdriver(Proxy)
 
 	try :
 		driver.get(SiteUrl)
-		driver.implicitly_wait(10)
-		time.sleep(random.randint(1, 3))
-		page_html = driver.page_source
-		
-		#상품설명 추가
-		detail_url = ''
-		detail_html = ''
+		driver.implicitly_wait(5)
+		PageHtml = driver.page_source
+
+		NowUrl = driver.current_url
+		"""
+		ItemInfoMatched = re.search('\{"actionModule".*\}', PageHtml)
+		ItemInfoData = ItemInfoMatched.group()
+
+		ItemInfo = json.loads(ItemInfoData)
+
+
+		DetailHtml = ''
+		DetailUrl = ItemInfo['descriptionModule']['descriptionUrl']
+
+		driver.get(DetailUrl)
+		driver.implicitly_wait(5)
+		DetailHtml = driver.page_source
+		"""
+
+		data = {'NtosServer':str(NtosServer), 'NotsKey':NotsKey, 'CustId':CustId, 'SlId':SlId, 'PageHtml':PageHtml, 'log_id': log_id, 'NowUrl':NowUrl }
+		headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+		Result_ = ""
 		try :
-			driver.get(detail_url)
-			driver.implicitly_wait(10)
-			time.sleep(random.randint(1, 3))
-			detail_html = driver.page_source
+			Result__ = requests.post(NtosServer, data=json.dumps(data), headers=headers)
+			Result_ = Result__.text
 		except :
-			detail_url = ''
-			detail_html = ''
-		
-		if page_html :
-
-			data = {'NtosServer':str(NtosServer), 'NotsKey':NotsKey, 'CustId':CustId, 'SlId':SlId, 'PageHtml':page_html, 'DetailHtml':detail_html }
-			headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-
-			Result_ = ""
-			try :
-				Result__ = requests.post(NtosServer, data=json.dumps(data), headers=headers)
-				Result_ = Result__.text
-			except :
-				Result_ = "error"
-		else :
 			Result_ = "error"
 
+
 		print(Result_)
+
 		driver.close()
 		driver.quit()
+
 	except :
+		data = {'NtosServer':str(NtosServer), 'NotsKey':NotsKey, 'CustId':CustId, 'SlId':SlId, 'PageHtml':'', 'log_id': log_id, 'NowUrl':'' }
+		headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+		Result__ = requests.post(NtosServer, data=json.dumps(data), headers=headers)
 		print("except")
 		driver.close()
 		driver.quit()
 
 
+
 if __name__ == '__main__':
 	pool = multiprocessing.Pool(processes=len(process_list))
 	pool.map(multiSelenium, process_list)
-	#print("\n\n--- %s seconds ---" % (time.time() - start_time))
+	print("\n\n--- %s seconds ---" % (time.time() - start_time))
 	pool.close()
 	pool.join()
 	sys.exit()
-
