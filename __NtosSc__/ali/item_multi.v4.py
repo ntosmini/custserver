a39aee2c02cd0184a2ecb8ce4e47d370a06c83c3
jungmini <mins0428@gmail.com>
2022-10-06 15:26:41 +0900

    Update item_multi.v4.py

--- __NtosSc__/ali/item_multi.v4.py
+++ __NtosSc__/ali/item_multi.v4.py
@@ -27,20 +27,7 @@ try :
 except :
 	pass
 
-process_list = []
-"""
-MConfigData = sys.argv[1]
-MConfig = json.loads(MConfigData)
 
-NtosServer = MConfig['NtosServer']
-process_list = MConfig['SlId_SiteUrl']
-NotsKey = MConfig['NotsKey']
-CustId = MConfig['CustId']
-"""
-NtosServer = "http://ali3.ntos.co.kr/__NtosWb__/ali/Multi_AliItemScrap.get.php"
-process_list = ['47155521111|@|https://ko.aliexpress.com/item/2255801033533139.html|@|20548', '47156121111|@|https://ko.aliexpress.com/item/3256802591122424.html|@|20549', '47156721111|@|https://ko.aliexpress.com/item/2251832758067607.html|@|20550']
-NotsKey = "79e687bedec9f439815c886c3445ded581ff2597"
-CustId = "aliexpress"
 
 start_time = time.time()
 
@@ -60,41 +47,12 @@ def chromeWebdriver(Proxy):
 
 	return driver
 
+driver = chromeWebdriver('')
+SiteUrl = "http://ntos.co.kr"
+driver.get(SiteUrl)
+driver.implicitly_wait(5)
+PageHtml = driver.page_sourc
 
-def multiSelenium(process):
-	(SlId, SiteUrl, log_id) = process.split("|@|")
-
-	driver = chromeWebdriver('')
-	PageHtml = ""
-	NowUrl = ""
-	try :
-		driver.get(SiteUrl)
-		driver.implicitly_wait(5)
-		PageHtml = driver.page_source
-
-		NowUrl = driver.current_url
-	except :
-		pass
-
-	data = {'NtosServer':str(NtosServer), 'NotsKey':NotsKey, 'CustId':CustId, 'SlId':SlId, 'PageHtml':PageHtml, 'log_id': log_id, 'NowUrl':NowUrl }
-	headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-
-	Result_ = ""
-	try :
-		Result__ = requests.post(NtosServer, data=json.dumps(data), headers=headers)
-		Result_ = Result__.text
-	except :
-		Result_ = "error"
-
-	print(Result_)
-	driver.close()
-	driver.quit()
-
-
-if __name__ == '__main__':
-	pool = multiprocessing.Pool(processes=len(process_list))
-	pool.map(multiSelenium, process_list)
-	print("\n\n--- %s seconds ---" % (time.time() - start_time))
-	pool.close()
-	pool.join()
-	sys.exit()
+print(PageHtml)
+driver.close()
+driver.quit()
