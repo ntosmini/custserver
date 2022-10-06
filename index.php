acf685a224cb0fd7c604ec8ba05437bfc0c61770
jungmini <mins0428@gmail.com>
2022-10-07 07:59:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(empty($Type)){
 } else if($Type == "py"){
 	if($Ver == "v4" || $Ver == "v3"){
 		$RunData = array();
-		$RunData['SiteUrl'] = "http://mini.ntos.co.kr/_se_chk.html";
+		$RunData['SiteUrl'] = (empty($_GET['SiteUrl']))?"http://mini.ntos.co.kr/_se_chk.html":$_GET['SiteUrl'];
 		$MConfigData = escapeshellarg(json_encode($RunData));
 		exec("python3 /home/ntosmini/public_html/__NtosSc__/_test.{$Ver}.py {$MConfigData}", $ResultArr);
 		$PageHtml = implode("\n", $ResultArr);
