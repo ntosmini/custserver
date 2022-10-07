42afd1919f208ff516d11339b5368865055a0b54
jungmini <mins0428@gmail.com>
2022-10-07 11:07:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import io
 import os
 import multiprocessing
 import requests
+import traceback
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -23,16 +24,19 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 executable_path=ChromeDriverManager().install()
 
-chrome_service = ChromeService(executable_path)
-chrome_options = Options()
-chrome_options.add_experimental_option('detach', True)
-chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument("window-size=1920,1080")
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+try :
+	chrome_service = ChromeService(executable_path)
+	chrome_options = Options()
+	chrome_options.add_experimental_option('detach', True)
+	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument("window-size=1920,1080")
+	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+except Exception:
+    err = traceback.format_exc()
+    print(str(err))
 
 print(executable_path)
 print("")
-driver.close()
-driver.quit()
