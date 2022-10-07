7e6ea2f54de199f08eea6a522078bc97fc4b384d
jungmini <mins0428@gmail.com>
2022-10-07 10:59:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -22,20 +22,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 executable_path=ChromeDriverManager().install()
-chrome_service = ChromeService(executable_path)
-chrome_options = Options()
-chrome_options.add_experimental_option('detach', True)
-chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument("window-size=1920,1080")
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-SiteUrl = "http://ntos.co.kr"
-driver.get(SiteUrl)
-driver.implicitly_wait(10)
-page_html = driver.page_source
-print(page_html)
 print(executable_path)
 driver.close()
 driver.quit()
