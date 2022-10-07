49d87c04180efaaae1fedefab14ee8f2e0e8a278
jungmini <mins0428@gmail.com>
2022-10-07 14:21:34 +0900

    Update test.php

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ header("Content-Type: text/html; charset=UTF-8");
 
 
 
-exec("python3 /home/ntosmini/public_html/test.php", $ResultArr);
+exec("python3 /home/ntosmini/public_html/test.py", $ResultArr);
 $PageHtml = implode("\n", $ResultArr);
 
 echo $PageHtml;
