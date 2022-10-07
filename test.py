9578f2fbc7160dfa0225cdcafb06c446f8de7927
jungmini <mins0428@gmail.com>
2022-10-07 10:41:30 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*- 
+# 테스트
+
+import time
+import sys
+import json
+import io
+import os
+import multiprocessing
+import requests
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+
+#pip3 install webdriver_manager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+#한글깨짐
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+executable_path=ChromeDriverManager().install()
+chrome_service = ChromeService(executable_path)
+print(executable_path)
