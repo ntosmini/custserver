06475309d11c8fb94fda75d7b7a8b36e250623da
jungmini <mins0428@gmail.com>
2022-10-12 16:27:56 +0900

    Update category_multi.v4.py

--- __NtosSc__/ali/category_multi.v4.py
+++ __NtosSc__/ali/category_multi.v4.py
@@ -61,7 +61,7 @@ def chromeWebdriver():
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--disable-gpu')
-	chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+	#chrome_options.add_argument("--blink-settings=imagesEnabled=false")
 	chrome_options.add_argument("window-size=1920,1080")
 
 	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
