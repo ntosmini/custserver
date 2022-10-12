ddddd545975de2ac3f628fb3bbbbf05299221846
jungmini <mins0428@gmail.com>
2022-10-12 09:13:54 +0900

    Update index.php

--- __NtosSc__/index.php
+++ __NtosSc__/index.php
@@ -1,4 +1,15 @@
 <?php
+set_time_limit(0);
+header("Content-Type: text/html; charset=UTF-8");
 /*
 Scrap 폴더
 */
+$Ver = (empty($_POST['Ver']))?"":$_POST['Ver'];	//v3 or v4
+$SiteUrl = $_POST['SiteUrl'];
+
+if(empty($SiteUrl)){
+  $PageHtml = "not SiteUrl"; 
+} else {
+  exec("python3 /home/ntosmini/public_html/Scrap.{$Ver}.py {$SiteUrl}", $ResultArr);
+  $PageHtml = implode("\n", $ResultArr);
+}
