288bcc5e30f5555ac89d45056fbafb21ff862f55
jungmini <mins0428@gmail.com>
2022-10-19 08:20:22 +0900

    Update _CategoryMulti.v3.py

--- __NtosSc__/ali/_CategoryMulti.v3.py
+++ __NtosSc__/ali/_CategoryMulti.v3.py
@@ -99,8 +99,15 @@ def multiSelenium(process):
 		Source = driver.page_source
 		NowUrl = driver.current_url
 
-		ScriptMatched = re.search('window._dida_config_._init_data_=.*?</script>', Source)
-		PageHtml = ScriptMatched.group()
+		ScriptMatched_1 = re.search(r'(?P<PageHtml>window._dida_config_._init_data_=.*(</script>){1})', Source, re.DOTALL)
+		ScriptMatched_2 = re.search(r'(?P<PageHtml>window.runParams =.*(</script>){1})', Source, re.DOTALL)
+		if ScriptMatched_1 :
+			PageHtml = ScriptMatched_1.group('PageHtml')
+		elif ScriptMatched_2 :
+			PageHtml = ScriptMatched_2.group('PageHtml')
+		else :
+			PageHtml = ""
+		
 	except :
 		pass
 
@@ -117,7 +124,6 @@ def multiSelenium(process):
 		Result_ = "error"
 
 	print(Result_)
-	driver.close()
 	driver.quit()
 
 
