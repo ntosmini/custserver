70d92bf0ffe861f8e00796e41bbfaa2dae60e945
jungmini <mins0428@gmail.com>
2022-11-08 15:24:00 +0900

    Add files via upload

---
+++ _NtosSc_/ali/_ItemStraight.v4.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*- 
+#상품 스트레이트-v4
+
+import time
+import sys
+import json
+import io
+import os
+import multiprocessing
+import requests
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+
+#pip3 install webdriver_manager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+#한글깨짐
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+try :
+	os.system("killall -o 3m chrome")
+	os.system("killall -o 3m chromedriver")
+except :
+	pass
+
+IslId_SiteUrl = []
+
+
+MConfigData = sys.argv[1]
+MConfig = json.loads(MConfigData)
+
+NtosServer = MConfig['NtosServer']
+IslId_SiteUrl = MConfig['IslId_SiteUrl']
+NotsKey = MConfig['NotsKey']
+CustId = MConfig['CustId']
+TimeChk = MConfig['TimeChk']
+LogChkUrl = MConfig['LogChkUrl']
+
+
+if TimeChk == "Y" :
+	start_time = time.time()
+
+executable_path = ChromeDriverManager().install()
+
+def chromeWebdriver():
+	chrome_service = ChromeService(executable_path)
+	chrome_options = Options()
+	chrome_options.add_experimental_option('detach', True)
+	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+	chrome_options.add_argument("window-size=1920,1080")
+	
+	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+	return driver
+
+driver = chromeWebdriver()
+
+for val in IslId_SiteUrl :
+	(IslId, SiteUrl, log_id) = val.split("|@|")
+
+	if LogChkUrl :
+		try :
+			requests.post(LogChkUrl, data=json.dumps({'CustId':CustId, 'log_id':log_id, 'Step':'1' }), headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
+		except :
+			pass
+
+	PageHtml = ""
+	NowUrl = ""
+	try :
+		driver.get(SiteUrl)
+		driver.implicitly_wait(5)
+		PageHtml = driver.page_source
+		NowUrl = driver.current_url
+		
+
+		if LogChkUrl :
+			try :
+				requests.post(LogChkUrl, data=json.dumps({'CustId':CustId, 'log_id':log_id, 'Step':'2' }), headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
+			except :
+				pass
+		
+	except :
+		pass
+
+	data = {'NtosServer':str(NtosServer), 'NotsKey':NotsKey, 'CustId':CustId, 'IslId':IslId, 'PageHtml':str(PageHtml), 'log_id': log_id, 'NowUrl':str(NowUrl) }
+	headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+	Result_ = ""
+	try :
+		Result__ = requests.post(NtosServer, data=json.dumps(data), headers=headers)
+		Result_ = Result__.text
+		
+		if LogChkUrl :
+			try :
+				requests.post(LogChkUrl, data=json.dumps({'CustId':CustId, 'log_id':log_id, 'Step':'3' }), headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
+			except :
+				pass
+		
+	except :
+		Result_ = "error"
+
+	print(Result_)
+	time.sleep(2)
+
+driver.quit()
