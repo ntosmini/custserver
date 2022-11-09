5b75ddbd83e27f07b2f824a7e7dd3e6efe031bda
jungmini <mins0428@gmail.com>
2022-11-10 00:22:14 +0900

    Update _ItemStraight.v4.py

--- _NtosSc_/ali/_ItemStraight.v4.py
+++ _NtosSc_/ali/_ItemStraight.v4.py
@@ -40,6 +40,7 @@ NotsKey = MConfig['NotsKey']
 CustId = MConfig['CustId']
 TimeChk = MConfig['TimeChk']
 LogChkUrl = MConfig['LogChkUrl']
+chk_idx = MConfig['chk_idx']
 
 if TimeChk == "Y" :
 	start_time = time.time()
@@ -98,7 +99,7 @@ for val in IslId_SiteUrl :
 		print("except")
 		pass
 
-	data = {'NtosServer':str(NtosServer), 'NotsKey':NotsKey, 'CustId':CustId, 'IslId':IslId, 'PageHtml':str(PageHtml), 'log_id': log_id, 'NowUrl':str(NowUrl) }
+	data = {'NtosServer':str(NtosServer), 'NotsKey':NotsKey, 'CustId':CustId, 'IslId':IslId, 'PageHtml':str(PageHtml), 'log_id': log_id, 'chk_idx': chk_idx, 'NowUrl':str(NowUrl) }
 	headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
 	Result_ = ""
