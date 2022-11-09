bae7b0a761e8a166c8b82a1154c100597a6fea44
jungmini <mins0428@gmail.com>
2022-11-09 10:35:30 +0900

    Update _ItemStraight.v3.py

--- _NtosSc_/ali/_ItemStraight.v3.py
+++ _NtosSc_/ali/_ItemStraight.v3.py
@@ -9,6 +9,7 @@ import os
 import re
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
