8e51d3a42c28730da31493cf5a281f6f3ed416bb
jungmini <mins0428@gmail.com>
2022-11-09 14:47:06 +0900

    Update _ItemStraight.v4.py

--- _NtosSc_/ali/_ItemStraight.v4.py
+++ _NtosSc_/ali/_ItemStraight.v4.py
@@ -69,6 +69,10 @@ def chromeWebdriver():
 
 driver = chromeWebdriver()
 
+if WaitChk == "Y" :
+	# 기본적으로 10초를 기다리고 다음 스크립트 실행
+	wait = WebDriverWait(driver, 10, 2)
+
 for val in IslId_SiteUrl :
 	(IslId, SiteUrl, log_id) = val.split("|@|")
 
@@ -84,11 +88,9 @@ for val in IslId_SiteUrl :
 	try :
 		driver.get(SiteUrl)
 		if WaitChk == "Y" :
-			# 기본적으로 10초를 기다리고 다음 스크립트 실행
-			wait = WebDriverWait(driver, 10, 2)
-			# logo-base 클래스가 나타날때까지 기다린다.
+			# product-title-text 클래스가 나타날때까지 기다린다.
 			wait.until(
-				EC.presence_of_element_located((By.CLASS_NAME, "logo-base"))
+				EC.presence_of_element_located((By.CLASS_NAME, "product-title-text"))
 			)
 			# javascript 실행을 중지시킨다.
 			driver.execute_script("window.stop();")
