bcce73ff7297cc1076229211567fcc9da6ed9b3a
jungmini <mins0428@gmail.com>
2022-11-24 14:23:46 +0900

    Update _test.v4.py

--- _test.v4.py
+++ _test.v4.py
@@ -26,11 +26,51 @@ MConfigData = sys.argv[1]
 MConfig = json.loads(MConfigData)
 
 SiteUrl = MConfig['SiteUrl']
+refresh = MConfig['refresh']
+log = MConfig['log']
 
 start_time = time.time()
 
+
+logfile = "/home/ntosmini/public_html/log/"+time.strftime('%Y%m%d', time.localtime(time.time()))+".txt"
+
+def logsave(text) :
+	if log == "n" :
+		return
+	
+	filechk = ''
+	logdata = ''
+	if os.path.isfile(logfile):
+		f = open(logfile, 'r', encoding="utf8")
+		logdata = f.read()
+		f.close()
+		filechk = 'y'
+	else :
+		filechk = 'n'
+
+	if text :
+		val = ""
+		chktime = str( round((time.time() - start_time), 2))
+		if text == "start" :
+			val = "============================= start "+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+		elif text == "end" :
+			val = "\n============================= end "+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+" >> "+chktime+" seconds \n\n\n"
+		else :
+			val = "\n"+str(text)+" >> "+chktime+" seconds"
+
+		f = open(logfile, 'w', encoding="utf8")
+		f.write(str(logdata)+str(val))
+		f.close()
+
+
+logsave("start")
+logsave("ChromeService 체크전")
+executable_path = ChromeDriverManager().install()
+logsave("ChromeService 체크완료")
+
 def chromeWebdriver():
-	chrome_service = ChromeService(executable_path=ChromeDriverManager().install())
+	chrome_service = ChromeService(executable_path)
 	chrome_options = Options()
 	chrome_options.add_experimental_option('detach', True)
 	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -41,18 +81,29 @@ def chromeWebdriver():
 
 	return driver
 
+logsave("chromeWebdriver 실행전")
 driver = chromeWebdriver()
+logsave("chromeWebdriver 실행완료")
 try :
+	logsave("chromeWebdriver 이동전")
 	driver.get(SiteUrl)
 	driver.implicitly_wait(10)
-	driver.refresh()
-	driver.implicitly_wait(10)
+	logsave("chromeWebdriver 이동완료")
+	if refresh == "y" :
+		logsave("refresh 전")
+		driver.refresh()
+		driver.implicitly_wait(10)
+		logsave("refresh 완료")
+	logsave("source 불러오기전")
 	page_html = driver.page_source
+	logsave("source 불러오기 완료")
 	print(page_html)
 	driver.close()
 	driver.quit()
 except :
 	err = traceback.format_exc()
+	logsave("try except")+str(err)
 	print(str(err))
 	driver.close()
 	driver.quit()
+logsave("end")
