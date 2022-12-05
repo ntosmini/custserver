3cbfa82855315b543dd7d8194f612b6847328995
jungmini <mins0428@gmail.com>
2022-12-05 15:35:19 +0900

    Add files via upload

--- _AliSc_/Category.v4.py
+++ _AliSc_/Category.v4.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*- 
-#상품 스트레이트-v4
+# 카테고리 멀티-v4
 
 import time
 import sys
 import json
 import io
 import os
+
+#한글깨짐
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+
+try :
+	os.system("killall -o 3m chrome")
+	os.system("killall -o 3m chromedriver")
+except :
+	pass
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -17,31 +28,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-#한글깨짐
-sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
-
-try :
-	os.system("killall -o 3m chrome")
-	os.system("killall -o 3m chromedriver")
-except :
-	pass
-
-IslId_SiteUrl = []
+CslId_SiteUrl = []
 
 
 MConfigData = sys.argv[1]
 MConfig = json.loads(MConfigData)
 
-IslId_SiteUrl = MConfig['IslId_SiteUrl']
+CslId_SiteUrl = MConfig['CslId_SiteUrl']
 CustId = MConfig['CustId']
+Scroll = MConfig['Scroll']
 
 
 FileDir = ""
 if CustId == "aliexpress" :
-	FileDir = "/home/ntosmini/ali_item_en/"
-elif CustId == "aliexpresskr" :
-	FileDir = "/home/ntosmini/ali_item_kr/"
+	FileDir = "/home/ntosmini/ali_category/"
 else :
 	print("allerror")
 	exit()
@@ -65,32 +65,37 @@ def chromeWebdriver():
 	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 	chrome_options.add_argument('--disable-infobars')
 	chrome_options.page_load_strategy = 'normal'
+
 	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 	return driver
+
 
 driver = chromeWebdriver()
 
 wait = WebDriverWait(driver, 10, 1)
 
+driver.get("https://aliexpress.com")
+wait.until( EC.presence_of_element_located((By.CLASS_NAME, "logo-base")) )
+
 """
 파일명
-item_{IslId}.html
+item_{CslId}.html
 
 상단 내용++
 <ntosoriginurl></ntosoriginurl>
 <ntosnowurl></ntosnowurl>
 """
 
-for val in IslId_SiteUrl :
-	(IslId, SiteUrl) = val.split("|@|")
+for val in CslId_SiteUrl :
+	(CslId, SiteUrl) = val.split("|@|")
 
 	OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>"
 
 	#저장파일명
-	SaveFile = FileDir+"item_"+str(IslId)+".html"
+	SaveFile = FileDir+"category_"+str(CslId)+".html"
 
-	if IslId == "" or SiteUrl == "" :
+	if CslId == "" or SiteUrl == "" :
 		f = open(SaveFile, 'w', encoding="utf8")
 		f.write(OriginUrl)
 		f.close()
@@ -101,14 +106,29 @@ for val in IslId_SiteUrl :
 			driver.get(SiteUrl)
 			wait.until( EC.presence_of_element_located((By.CLASS_NAME, "logo-base")) )
 			#driver.execute_script("window.stop();")
-			
+
+			if Scroll == "Y" :
+				SCROLL_PAUSE_SEC = 0.5
+				# 스크롤 높이 가져옴
+				last_height = driver.execute_script("return document.body.scrollHeight")
+
+				while True:
+					# 끝까지 스크롤 다운
+					driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+					# SCROLL_PAUSE_SEC 초 대기
+					time.sleep(SCROLL_PAUSE_SEC)
+
+					# 스크롤 다운 후 스크롤 높이 다시 가져옴
+					new_height = driver.execute_script("return document.body.scrollHeight")
+					if new_height == last_height:
+						break
+					last_height = new_height
 			PageHtml = driver.page_source
 			NowUrl = driver.current_url
-
 		except :
 			PageHtml = ""
 			NowUrl = ""
-
 
 		WriteFile = ""
 		WriteFile = WriteFile + OriginUrl+"\n"
--- _AliSc_/Item.v3.py
+++ _AliSc_/Item.v3.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*- 
-#상품 스트레이트-v4
+#상품 스트레이트-v3
 
 import time
 import sys
 import json
 import io
 import os
+import re
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-#pip3 install webdriver_manager
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 
 #한글깨짐
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
@@ -36,7 +32,6 @@ MConfig = json.loads(MConfigData)
 IslId_SiteUrl = MConfig['IslId_SiteUrl']
 CustId = MConfig['CustId']
 
-
 FileDir = ""
 if CustId == "aliexpress" :
 	FileDir = "/home/ntosmini/ali_item_en/"
@@ -50,26 +45,19 @@ else :
 def osgzip(File) :
 	os.system("gzip "+File)
 
-executable_path = ChromeDriverManager().install()
 
-def chromeWebdriver():
-	chrome_service = ChromeService(executable_path)
-	chrome_options = Options()
-	chrome_options.add_experimental_option('detach', True)
-	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-	chrome_options.add_argument('--headless')
-	chrome_options.add_argument('--no-sandbox')
-	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-	chrome_options.add_argument('window-size=1920,1080')
-	chrome_options.add_argument('--disable-dev-shm-usage')
-	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-	chrome_options.add_argument('--disable-infobars')
-	chrome_options.page_load_strategy = 'normal'
-	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("-disable-notifications")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+chrome_options.add_argument('--disable-infobars')
+chrome_options.page_load_strategy = 'normal'
 
-	return driver
-
-driver = chromeWebdriver()
+driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=chrome_options)
 
 wait = WebDriverWait(driver, 10, 1)
 
@@ -101,10 +89,9 @@ for val in IslId_SiteUrl :
 			driver.get(SiteUrl)
 			wait.until( EC.presence_of_element_located((By.CLASS_NAME, "logo-base")) )
 			#driver.execute_script("window.stop();")
-			
+
 			PageHtml = driver.page_source
 			NowUrl = driver.current_url
-
 		except :
 			PageHtml = ""
 			NowUrl = ""
