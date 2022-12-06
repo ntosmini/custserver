2fc0c8edd06b73c99d5f1ac6ac809f0ee010c23f
jungmini <mins0428@gmail.com>
2022-12-06 13:39:21 +0900

    Update Category.v3.py

--- _AliSc_/Category.v3.py
+++ _AliSc_/Category.v3.py
@@ -33,7 +33,6 @@ CslId_SiteUrl = MConfig['CslId_SiteUrl']
 CustId = MConfig['CustId']
 Scroll = MConfig['Scroll']
 ScrapServerId = MConfig['ScrapServerId']
-LogId = MConfig['LogId']
 
 
 FileDir = ""
@@ -73,7 +72,7 @@ category_{CustId}_{CslId}_{server_id}_{LogId}.html
 """
 
 for val in CslId_SiteUrl :
-	(CslId, SiteUrl) = val.split("|@|")
+	(CslId, SiteUrl, LogId) = val.split("|@|")
 
 	OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>"
 
