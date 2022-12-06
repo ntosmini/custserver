8c22d3e3f09f19cb027f770d1e401931b16bc9d2
jungmini <mins0428@gmail.com>
2022-12-06 10:36:31 +0900

    Update Category.v3.py

--- _AliSc_/Category.v3.py
+++ _AliSc_/Category.v3.py
@@ -64,7 +64,7 @@ wait.until( EC.presence_of_element_located((By.CLASS_NAME, "logo-base")) )
 
 """
 파일명
-item_{CustId}_{CslId}_{server_id}.html
+category_{CustId}_{CslId}_{server_id}.html
 
 상단 내용++
 <ntosoriginurl></ntosoriginurl>
