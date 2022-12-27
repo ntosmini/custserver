ac2abf3fe928853a0289078fe66876b221963905
jungmini <mins0428@gmail.com>
2022-12-27 09:57:02 +0900

    Update _ServerChk.php

--- _AliSc_/_ServerChk.php
+++ _AliSc_/_ServerChk.php
@@ -9,7 +9,8 @@ $Type = (empty($_POST['Type']))?"":$_POST['Type'];
 $Ver = (empty($_POST['Ver']))?"v4":$_POST['Ver'];	//v3 or v4
 $RunData['CustId'] = (empty($_POST['CustId']))?"":$_POST['CustId'];
 $RunData['SiteUrl'] = (empty($_POST['SiteUrl']))?"http://product.ntos.co.kr/_SeleniumChk.php":$_POST['SiteUrl'];
-$RunData['NtosServer'] = (empty($_POST['NtosServer']))?"":$_POST['NtosServer'];	//받을 url
+$RunData['Server'] = (empty($_POST['Server']))?"":$_POST['Server'];	//통신 서버
+$RunData['ReturnUrl'] = (empty($_POST['ReturnUrl']))?"":$_POST['ReturnUrl'];	//받을 url
 $RunData['Sid'] = (empty($_POST['Sid']))?"":$_POST['Sid'];	//서버 ID
 
 
@@ -29,20 +30,32 @@ if(empty($Ver) || ($Ver != 'v3' && $Ver != 'v4') ){
 	exit;
 }	//end if(empty($Ver))
 
+if(empty($RunData['CustId'])){
+	echo 'not CustId';
+	exit;
+}
+
 if(empty($RunData['SiteUrl'])){
 	echo 'not SiteUrl';
 	exit;
 }
 
-if(empty($RunData['NtosServer'])){
-	echo 'not NtosServer';
+if(empty($RunData['Server'])){
+	echo 'not Server';
 	exit;
 }
 
-if(empty($RunData['CustId'])){
-	echo 'not CustId';
+if(empty($RunData['ReturnUrl'])){
+	echo 'not ReturnUrl';
 	exit;
 }
+
+if(empty($RunData['Sid'])){
+	echo 'not Sid';
+	exit;
+}
+
+
 
 $MConfigData = escapeshellarg(json_encode($RunData));
 
