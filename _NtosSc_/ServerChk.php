a4e4ccfea5fad4154c6d637664df59ea3b940e35
jungmini <mins0428@gmail.com>
2023-01-20 11:32:24 +0900

    Update ServerChk.php

--- _NtosSc_/ServerChk.php
+++ _NtosSc_/ServerChk.php
@@ -4,21 +4,17 @@ header("Content-Type: text/html; charset=UTF-8");
 
 $Type = (empty($_GET['Type']))?"web":$_GET['Type']; //web, selenium
 $Ver = (empty($_GET['Ver']))?"":$_GET['Ver'];	//v3 or v4
-$SiteUrl = urldecode($_GET['SiteUrl']);
+$SiteUrl = (empty($_GET['SiteUrl']))?"":urldecode($_GET['SiteUrl']);
 
 $PageHtml = '';
-if(empty($SiteUrl)){
-   $PageHtml = 'not SiteUrl';
-} else {
-  if($Type == "web"){
-    $PageHtml = 'ntoswebsuccess';
-  } else if($Type == "selenium"){
+if($Type == "web"){
+	$PageHtml = 'ntoswebsuccess';
+} else if($Type == "selenium"){
 	$RunData['SiteUrl'] = $SiteUrl;
 	$MConfigData = escapeshellarg(json_encode($RunData));
 	exec("python3 /home/ntosmini/public_html/_NtosSc_/ServerChk.{$Ver}.py {$MConfigData}", $ResultArr);
 	$PageHtml = implode("\n", $ResultArr);
-  } else {
-    $PageHtml = 'not Type';
-  }
+} else {
+	$PageHtml = 'not Type';
 }
 echo $PageHtml;
