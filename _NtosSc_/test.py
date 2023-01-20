a3eb5e80fe7a1e2ec9ced2be6a2910383aca8a19
jungmini <mins0428@gmail.com>
2023-01-20 10:24:53 +0900

    Create test.py

---
+++ _NtosSc_/test.py
@@ -0,0 +1,8 @@
+<?php
+set_time_limit(0);
+header("Content-Type: text/html; charset=UTF-8");
+
+exec("python3 /home/ntosmini/public_html/_NtosSc_/test.py", $ResultArr);
+$PageHtml = implode("\n", $ResultArr);
+
+echo $PageHtml;
