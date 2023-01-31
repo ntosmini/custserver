55cafc8840a3e1ff87dadd356b5372125bef5240
jungmini <mins0428@gmail.com>
2023-01-31 10:46:10 +0900

    Delete test.php

--- _NtosSc_/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-set_time_limit(0);
-header("Content-Type: text/html; charset=UTF-8");
-
-exec("python3 /home/ntosmini/public_html/_NtosSc_/test.py", $ResultArr);
-$PageHtml = implode("\n", $ResultArr);
-
-echo $PageHtml;
