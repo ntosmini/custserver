17da09a2288d176f7d85a709eab16fa14d734dcc
jungmini <mins0428@gmail.com>
2023-02-22 13:50:33 +0900

    Update Scrap.py

--- _Scrap_/Scrap.py
+++ _Scrap_/Scrap.py
@@ -45,9 +45,6 @@ ScrapResultType = MConfig['ScrapResultType']
 FileSaveDir = MConfig['FileSaveDir']
 NtosSendServer = MConfig['NtosSendServer']
 UserAgent = MConfig['UserAgent']
-if UserAgent == "" :
-	UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-
 
 executable_path = ChromeDriverManager().install()
 
