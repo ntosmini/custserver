ab836f48925fe0f360280612582539c9bf58e6d5
jungmini <mins0428@gmail.com>
2023-04-11 14:13:23 +0900

    Update _Lock.php

--- _AliSc_/_Lock.php
+++ _AliSc_/_Lock.php
@@ -7,7 +7,7 @@ $RunData = array();
 $RunData['UserAgent'] = (empty($_POST['UserAgent']))?"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36":$_POST['UserAgent'];
 $RunData['SiteUrl'] = (empty($_POST['SiteUrl']))?"":$_POST['SiteUrl'];	//사이트 url
 
-$RunData['LockChk'] = (empty($_POST['LockChk']))?"N":"Y";	//lock chk
+$RunData['LockChkUsed'] = (empty($_POST['LockChkUsed']))?"N":"Y";	//lock chk
 
 if(empty($RunData['SiteUrl'])){
 	echo 'not SiteUrl';
