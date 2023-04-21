18837f65ddcd5830005df7fea6819ea2190d74df
jungmini <mins0428@gmail.com>
2023-04-21 14:57:30 +0900

    Update Scrap.php

--- _Scrap_/Scrap.php
+++ _Scrap_/Scrap.php
@@ -20,8 +20,9 @@ if($ScrapType == "self"){
 		echo 'not SiteUrlOne';
 		exit;
 	}
+	$SiteUrlOne = urldecode($SiteUrlOne);
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $SiteUrlOne );
+	curl_setopt($ch, CURLOPT_URL, $SiteUrlOne);
 	curl_setopt($ch, CURLOPT_HEADER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
