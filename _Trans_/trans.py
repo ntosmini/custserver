2fa9602bf1c77a58aad739a631e98e27b942d068
jungmini <mins0428@gmail.com>
2023-04-24 16:00:25 +0900

    Create trans.py

---
+++ _Trans_/trans.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*- 
+#구글번역 
+
+import time
+import sys
+import json
+import io
+import os
+import requests
+import googletrans
+
+#한글깨짐
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+
+MConfigData = sys.argv[1]
+MConfig = json.loads(MConfigData)
+
+
+NtosServer = MConfig['NtosServer']
+CustId = MConfig['CustId']
+it_id = MConfig['it_id']
+TransStr = MConfig['TransStr']
+OrgField = MConfig['OrgField']
+TargetField = MConfig['TargetField']
+g_dest = MConfig['g_dest']
+g_src = MConfig['g_src']
+
+translator = googletrans.Translator()
+
+ResultStr = translator.translate(TransStr, dest = g_dest, src = g_src)
+
+data = {'CustId':str(CustId), 'it_id':str(it_id), 'OrgField':str(OrgField), 'TargetField':str(TargetField), 'TransStr':str(TransStr) 'ResultStr':str(ResultStr.text) }
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+requests.post(NtosServer, data=json.dumps(data), headers=headers)
+
+
