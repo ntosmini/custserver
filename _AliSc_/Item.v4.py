a666ec5b8a8ad0193a54bb8e1d88813ed07897cb
jungmini <mins0428@gmail.com>
2023-05-22 14:47:42 +0900

    Update Item.v4.py

--- _AliSc_/Item.v4.py
+++ _AliSc_/Item.v4.py
@@ -20,6 +20,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+import undetected_chromedriver as uc
+
 #한글깨짐
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
@@ -62,19 +64,18 @@ executable_path = ChromeDriverManager().install()
 
 def chromeWebdriver():
 	chrome_service = ChromeService(executable_path)
-	chrome_options = Options()
-	chrome_options.add_experimental_option('detach', True)
-	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+	chrome_options = uc.ChromeOptions()
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-	chrome_options.add_argument('window-size=1920,1080')
+	chrome_options.add_argument('--start-maximized')
 	chrome_options.add_argument('--disable-dev-shm-usage')
 	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 	chrome_options.add_argument('--disable-infobars')
-	chrome_options.add_argument('user-agent=' + UserAgent)
-	chrome_options.page_load_strategy = 'normal'
-	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+	chrome_options.add_argument('--ignore-certificate-errors')
+	chrome_options.add_argument('--ignore-ssl-errors=yes')
+	chrome_options.add_argument('--disable-gpu')
+	driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
 	return driver
 
 driver = chromeWebdriver()
