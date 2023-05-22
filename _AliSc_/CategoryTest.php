9432922f95e298429004af0f16de6d8077256545
jungmini <mins0428@gmail.com>
2023-05-22 15:32:32 +0900

    Create CategoryTest.php

---
+++ _AliSc_/CategoryTest.php
@@ -0,0 +1,40 @@
+<?php
+set_time_limit(0);
+header("Content-Type: text/html; charset=UTF-8");
+/*
+카테고리 - 스트레이트
+*/
+$RunData = array();
+$Ver = (empty($_POST['Ver']))?"v4":$_POST['Ver'];	//v3 or v4
+$RunData['CslId_SiteUrl'] = (empty($_POST['CslId_SiteUrl']))?"":$_POST['CslId_SiteUrl'];
+$RunData['CustId'] = (empty($_POST['CustId']))?"":$_POST['CustId'];
+$RunData['Scroll'] = (empty($_POST['Scroll']))?"N":$_POST['Scroll'];	//스크롤
+
+$RunData['FileSendSave'] = (empty($_POST['FileSendSave']))?"N":$_POST['FileSendSave'];	//파일 저장 전송 사용여부 Y/N
+$RunData['NtosServer'] = (empty($_POST['NtosServer']))?"":$_POST['NtosServer'];	//받을 url
+$RunData['UserAgent'] = (empty($_POST['UserAgent']))?"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36":$_POST['UserAgent'];
+
+if(empty($Ver) || ($Ver != 'v3' && $Ver != 'v4') ){
+	echo 'not Ver';
+	exit;
+}	//end if(empty($Ver))
+
+if(empty($RunData['CslId_SiteUrl'])){
+	echo 'not CslId_SiteUrl';
+	exit;
+}
+$RunData['CslId_SiteUrl'] = explode("|^|", $RunData['CslId_SiteUrl']);
+
+if(empty($RunData['CustId'])){
+	echo 'not CustId';
+	exit;
+}
+
+$MConfigData = escapeshellarg(json_encode($RunData));
+
+
+exec("python3 /home/ntosmini/public_html/_AliSc_/Category.{$Ver}.py {$MConfigData}", $ResultArr);
+
+$PageHtml = implode("\n", $ResultArr);
+
+echo $PageHtml;
