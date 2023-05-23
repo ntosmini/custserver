5e06cc32d6e8aed735b98d8ae246deb4e43ac0f8
jungmini <mins0428@gmail.com>
2023-05-24 08:59:01 +0900

    Update CategoryTest.php

--- _AliSc_/CategoryTest.php
+++ _AliSc_/CategoryTest.php
@@ -1,8 +1,12 @@
 <?php
 set_time_limit(0);
 header("Content-Type: text/html; charset=UTF-8");
-
-exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_AliSc_/CategoryTest.v4.py", $ResultArr);
+$PATH = (empty($_GET['path']))?"":$_GET['path'];
+if(empty($PATH)){
+  exec("python3 /home/ntosmini/public_html/_AliSc_/CategoryTest.v4.py", $ResultArr);
+} else {
+  exec($PATH." python3 /home/ntosmini/public_html/_AliSc_/CategoryTest.v4.py", $ResultArr);
+}
 
 $PageHtml = implode("\n", $ResultArr);
 
