fae2c040df55f85e08d2595516dc6565a292d93a
jungmini <mins0428@gmail.com>
2023-05-24 12:40:09 +0900

    Update _Lock.php

--- _AliSc_/_Lock.php
+++ _AliSc_/_Lock.php
@@ -17,7 +17,7 @@ if(empty($RunData['SiteUrl'])){
 $MConfigData = escapeshellarg(json_encode($RunData));
 
 if($ChromeType == "uc"){
-	exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/Scrap.uc.py {$MConfigData}", $ResultArr);		
+	exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/_Lock.uc.py {$MConfigData}", $ResultArr);		
 } else {
 	exec("python3 /home/ntosmini/public_html/_Scrap_/Scrap.py {$MConfigData}", $ResultArr);
 }
