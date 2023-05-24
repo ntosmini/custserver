b8851e34c6c9759dca41d74254b19fc97e1e2999
jungmini <mins0428@gmail.com>
2023-05-24 09:25:45 +0900

    Update CategoryTest.php

--- _AliSc_/CategoryTest.php
+++ _AliSc_/CategoryTest.php
@@ -7,9 +7,11 @@ $PATH = (empty($_GET['path']))?"":$_GET['path'];
 $RunData['PathUsed'] = (empty($PATH))?"N":"Y";
 $MConfigData = escapeshellarg(json_encode($RunData));
 if(empty($PATH)){
+  echo 'Selenium<br>';
+  echo 'undetected_chromedriver 사용시 ?path=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin<br><br>';
   exec("python3 /home/ntosmini/public_html/_AliSc_/CategoryTest.v4.py {$MConfigData}", $ResultArr);
 } else {
-  echo $PATH;
+  echo 'undetected_chromedriver<br>';
   exec("PATH=".$PATH." python3 /home/ntosmini/public_html/_AliSc_/CategoryTest.v4.py {$MConfigData}", $ResultArr);
 }
 
