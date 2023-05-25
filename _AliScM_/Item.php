f095c7751ceb31ce3a632332198a5c8fc316074e
jungmini <mins0428@gmail.com>
2023-05-25 10:03:02 +0900

    Update Item.php

--- _AliScM_/Item.php
+++ _AliScM_/Item.php
@@ -11,7 +11,7 @@ $RunData['CustId'] = (empty($_POST['CustId']))?"":$_POST['CustId'];
 $RunData['IslId_SiteUrl'] = (empty($_POST['IslId_SiteUrl']))?"":$_POST['IslId_SiteUrl'];
 $RunData['FileSendSave'] = (empty($_POST['FileSendSave']))?"N":$_POST['FileSendSave'];	//파일 저장 전송 사용여부 Y/N
 $RunData['NtosServer'] = (empty($_POST['NtosServer']))?"":$_POST['NtosServer'];	//받을 url
-$RunData['FileDir']  =(empty($_POST['FileDir ']))?"":$_POST['FileDir '];  //저장폴더
+$RunData['FileDir']  =(empty($_POST['FileDir']))?"":$_POST['FileDir'];  //저장폴더
 $RunData['UserAgent'] = (empty($_POST['UserAgent']))?"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36":$_POST['UserAgent'];
 
 
