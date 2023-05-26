c9b82e73ab37eb6dfbe31a9b82231a23cb536aa1
jungmini <mins0428@gmail.com>
2023-05-26 09:27:21 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,28 +5,23 @@ header("Content-Type: text/html; charset=UTF-8");
 셀레니움 실행 확인용
 */
 $Type = (empty($_GET['Type']))?"":$_GET['Type'];
-$UserAgent = (empty($_GET['UserAgent']))?"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36":$_GET['UserAgent'];
-echo $UserAgent."<br>";
+
+$RunData = array();
+$RunData['SiteUrl'] = (empty($_GET['SiteUrl']))?"http://product.ntos.co.kr/_SeleniumChk.php":urldecode($_GET['SiteUrl']);
+$RunData['UserAgent'] = (empty($_GET['UserAgent']))?"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36":$_GET['UserAgent'];
+$MConfigData = escapeshellarg(json_encode($RunData));
+
 if(empty($Type)){
 	$PageHtml = "<div>ntoswebsuccess</div>";
 	$PageHtml .= "<div>Type=(se or uc)</div>";
 	$PageHtml .= "<div>SiteUrl=</div>";
 } else if($Type == "curl"){
-	$RunData = array();
-	$RunData['SiteUrl'] = (empty($_GET['SiteUrl']))?"http://product.ntos.co.kr/_SeleniumChk.php":urldecode($_GET['SiteUrl']);
-	$MConfigData = escapeshellarg(json_encode($RunData));
 	exec("python3 /home/ntosmini/public_html/index.curl.py {$MConfigData}", $ResultArr);
 	$PageHtml = implode("\n", $ResultArr);
 } else if($Type == "se"){
-	$RunData = array();
-	$RunData['SiteUrl'] = (empty($_GET['SiteUrl']))?"http://product.ntos.co.kr/_SeleniumChk.php":urldecode($_GET['SiteUrl']);
-	$MConfigData = escapeshellarg(json_encode($RunData));
 	exec("python3 /home/ntosmini/public_html/index.se.py {$MConfigData}", $ResultArr);
 	$PageHtml = implode("\n", $ResultArr);
 } else if($Type == "uc"){
-	$RunData = array();
-	$RunData['SiteUrl'] = (empty($_GET['SiteUrl']))?"http://product.ntos.co.kr/_SeleniumChk.php":urldecode($_GET['SiteUrl']);
-	$MConfigData = escapeshellarg(json_encode($RunData));
 	exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/index.uc.py {$MConfigData}", $ResultArr);
 	$PageHtml = implode("\n", $ResultArr);
 } else {
