a52d7f5be72877bf09108e11429188ae0754a651
jungmini <mins0428@gmail.com>
2023-06-01 08:26:22 +0900

    Update Category.v4.py

--- _AliSc_/Category.v4.py
+++ _AliSc_/Category.v4.py
@@ -20,6 +20,9 @@ try :
 except :
 	pass
 
+import undetected_chromedriver as uc
+from fake_useragent import UserAgent
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
@@ -30,7 +33,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-import undetected_chromedriver as uc
 
 CslId_SiteUrl = []
 
@@ -57,22 +59,23 @@ def osgzip(File) :
 	os.system("gzip "+File)
 
 def chromeWebdriver():
+	ua = UserAgent()
 	chrome_service = ChromeService(ChromeDriverManager().install())
-	chrome_options = uc.ChromeOptions()
+	chrome_options = Options()
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-	chrome_options.add_argument('--start-maximized')
+	chrome_options.add_argument('--window-size=1920,1080')
 	chrome_options.add_argument('--disable-dev-shm-usage')
-	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+	chrome_options.add_argument('--disable-blink-features=AutomationControlled') # 이걸로도 되네?
 	chrome_options.add_argument('--disable-infobars')
-	chrome_options.add_argument('--ignore-certificate-errors')
-	chrome_options.add_argument('--ignore-ssl-errors=yes')
-	chrome_options.add_argument('--disable-gpu')
-	driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
+	chrome_options.add_argument('--user-agent=' + ua.random)
+	chrome_options.page_load_strategy = 'normal'
+	driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=113)
 	return driver
 
 driver = chromeWebdriver()
+driver.delete_all_cookies()
 
 driver.get("https://aliexpress.com")
 
