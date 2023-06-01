34432791080557525595b3018642106855c69000
jungmini <mins0428@gmail.com>
2023-06-01 09:37:44 +0900

    Update Category.v4.py

--- _AliSc_/Category.v4.py
+++ _AliSc_/Category.v4.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 # 카테고리 파일-v4
 
 import time
@@ -15,10 +15,10 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 
 try :
-    os.system("killall -o 3m chrome")
-    os.system("killall -o 3m chromedriver")
+	os.system("killall -o 3m chrome")
+	os.system("killall -o 3m chromedriver")
 except :
-    pass
+	pass
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -48,29 +48,29 @@ UserAgent = MConfig['UserAgent']
 
 FileDir = ""
 if CustId == "aliexpress" :
-    FileDir = "/home/ntosmini/ali_category/"
+	FileDir = "/home/ntosmini/ali_category/"
 else :
-    exit()
+	exit()
 
 
 def osgzip(File) :
-    os.system("gzip "+File)
+	os.system("gzip "+File)
 
 def chromeWebdriver():
-    chrome_service = ChromeService(ChromeDriverManager().install())
-    chrome_options = uc.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-    chrome_options.add_argument('--start-maximized')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_argument('--disable-infobars')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--ignore-ssl-errors=yes')
-    chrome_options.add_argument('--disable-gpu')
-    driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
-    return driver
+	chrome_service = ChromeService(ChromeDriverManager().install())
+	chrome_options = uc.ChromeOptions()
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+	chrome_options.add_argument('--start-maximized')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+	chrome_options.add_argument('--disable-infobars')
+	chrome_options.add_argument('--ignore-certificate-errors')
+	chrome_options.add_argument('--ignore-ssl-errors=yes')
+	chrome_options.add_argument('--disable-gpu')
+	driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
+	return driver
 
 driver = chromeWebdriver()
 
@@ -87,56 +87,56 @@ cate_{CustId}_{CslId}_{CaId}_{server_id}_{LogId}.html
 """
 
 for val in CslId_SiteUrl :
-    (SiteUrl, SaveFileName) = val.split("|@|")
-    OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>"
-    #저장파일명
-    SaveFile = FileDir+str(SaveFileName)
-    ErrHtml = ''
-    if SiteUrl == "" or SaveFileName == "" :
-        continue
-    else :
-        try :
-            driver.get(SiteUrl)
-            #driver.execute_script("window.stop();")
-            if Scroll == "Y" :
-                SCROLL_PAUSE_SEC = 0.5
-                # 스크롤 높이 가져옴
-                last_height = driver.execute_script("return document.body.scrollHeight")
-                while True:
-                    # 끝까지 스크롤 다운
-                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                    # SCROLL_PAUSE_SEC 초 대기
-                    time.sleep(SCROLL_PAUSE_SEC)
-                    # 스크롤 다운 후 스크롤 높이 다시 가져옴
-                    new_height = driver.execute_script("return document.body.scrollHeight")
-                    if new_height == last_height:
-                        break
-                    last_height = new_height
-            PageHtml = driver.page_source
-            NowUrl = driver.current_url
-        except :
-            PageHtml = ""
-            NowUrl = ""
-            ErrHtml = traceback.format_exc()
-
-        WriteFile = ""
-        WriteFile = WriteFile + OriginUrl+"\n"
-        if NowUrl :
-            WriteFile = WriteFile + "<ntosnowurl>"+NowUrl+"</ntosnowurl>\n"
-        WriteFile = WriteFile + PageHtml
-        if ErrHtml :
-            WriteFile = WriteFile + "\n\n" + str(ErrHtml)
-        f = open(SaveFile, 'w', encoding="utf8")
-        f.write(WriteFile)
-        f.close()
-        osgzip(SaveFile)
-        if FileSendSave == "Y" and NtosServer != "" :
-            gzfile = SaveFile+".gz"
-            files = open(gzfile, 'rb')
-            upload = {'file': files}
-            data = {'CustId':CustId, 'ScrapType':'cate' }
-            Result_ = requests.post(NtosServer, data=data, files=upload)
-            Result = Result_.text
-            if os.path.exists(gzfile) :
-                os.remove(gzfile)
+	(SiteUrl, SaveFileName) = val.split("|@|")
+	OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>"
+	#저장파일명
+	SaveFile = FileDir+str(SaveFileName)
+	ErrHtml = ''
+	if SiteUrl == "" or SaveFileName == "" :
+		continue
+	else :
+		try :
+			driver.get(SiteUrl)
+			#driver.execute_script("window.stop();")
+			if Scroll == "Y" :
+				SCROLL_PAUSE_SEC = 0.5
+				# 스크롤 높이 가져옴
+				last_height = driver.execute_script("return document.body.scrollHeight")
+				while True:
+					# 끝까지 스크롤 다운
+					driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+					# SCROLL_PAUSE_SEC 초 대기
+					time.sleep(SCROLL_PAUSE_SEC)
+					# 스크롤 다운 후 스크롤 높이 다시 가져옴
+					new_height = driver.execute_script("return document.body.scrollHeight")
+					if new_height == last_height:
+						break
+					last_height = new_height
+			PageHtml = driver.page_source
+			NowUrl = driver.current_url
+		except :
+			PageHtml = ""
+			NowUrl = ""
+			ErrHtml = traceback.format_exc()
+			
+		WriteFile = ""
+		WriteFile = WriteFile + OriginUrl+"\n"
+		if NowUrl :
+			WriteFile = WriteFile + "<ntosnowurl>"+NowUrl+"</ntosnowurl>\n"
+		WriteFile = WriteFile + PageHtml
+		if ErrHtml :
+			WriteFile = WriteFile + "\n\n" + str(ErrHtml)
+		f = open(SaveFile, 'w', encoding="utf8")
+		f.write(WriteFile)
+		f.close()
+		osgzip(SaveFile)
+		if FileSendSave == "Y" and NtosServer != "" :
+			gzfile = SaveFile+".gz"
+			files = open(gzfile, 'rb')
+			upload = {'file': files}
+			data = {'CustId':CustId, 'ScrapType':'cate' }
+			Result_ = requests.post(NtosServer, data=data, files=upload)
+			Result = Result_.text
+			if os.path.exists(gzfile) :
+				os.remove(gzfile)
 driver.quit()
