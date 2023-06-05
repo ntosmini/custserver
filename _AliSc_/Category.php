9ea961651f709f673ea210b45402ac12c55b3cb3
jungmini <mins0428@gmail.com>
2023-06-05 16:34:19 +0900

    Update Category.php

--- _AliSc_/Category.php
+++ _AliSc_/Category.php
@@ -5,7 +5,6 @@ header("Content-Type: text/html; charset=UTF-8");
 카테고리 - 스트레이트
 */
 $RunData = array();
-$Ver = (empty($_POST['Ver']))?"v4":$_POST['Ver'];	//v3 or v4
 $RunData['CslId_SiteUrl'] = (empty($_POST['CslId_SiteUrl']))?"":$_POST['CslId_SiteUrl'];
 $RunData['CustId'] = (empty($_POST['CustId']))?"":$_POST['CustId'];
 $RunData['Scroll'] = (empty($_POST['Scroll']))?"N":$_POST['Scroll'];	//스크롤
@@ -13,11 +12,7 @@ $RunData['Scroll'] = (empty($_POST['Scroll']))?"N":$_POST['Scroll'];	//스크롤
 $RunData['FileSendSave'] = (empty($_POST['FileSendSave']))?"N":$_POST['FileSendSave'];	//파일 저장 전송 사용여부 Y/N
 $RunData['NtosServer'] = (empty($_POST['NtosServer']))?"":$_POST['NtosServer'];	//받을 url
 $RunData['UserAgent'] = (empty($_POST['UserAgent']))?"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36":$_POST['UserAgent'];
-
-if(empty($Ver) || ($Ver != 'v3' && $Ver != 'v4') ){
-	echo 'not Ver';
-	exit;
-}	//end if(empty($Ver))
+$RunData['CookiesLang'] = (empty($_POST['CookiesLang']))?"":$_POST['CookiesLang'];	//쿠키 언어
 
 if(empty($RunData['CslId_SiteUrl'])){
 	echo 'not CslId_SiteUrl';
@@ -33,4 +28,4 @@ if(empty($RunData['CustId'])){
 $MConfigData = escapeshellarg(json_encode($RunData));
 
 
-exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_AliSc_/Category.{$Ver}.py {$MConfigData}");
+exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_AliSc_/Category.py {$MConfigData}");
