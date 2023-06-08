bb16deea1f3342a0b4efd70288765d5011d595eb
jungmini <mins0428@gmail.com>
2023-06-08 14:00:25 +0900

    Update cookiestest.php

--- _AliSc_/cookiestest.php
+++ _AliSc_/cookiestest.php
@@ -4,8 +4,8 @@ header("Content-Type: text/html; charset=UTF-8");
 /*
 카테고리 - 스트레이트
 */
-exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_AliSc_/cookiestest.py, $ResultArr);
-
+//exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_AliSc_/cookiestest.py, $ResultArr);
+echo "a";
 $PageHtml = implode("\n", $ResultArr);
 
 echo $PageHtml;
