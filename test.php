092d001fec705ddae46cd5128748a2135b3e8cf0
jungmini <mins0428@gmail.com>
2023-06-09 10:00:42 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
 <?php
 set_time_limit(0);
 header("Content-Type: text/html; charset=UTF-8");
+$ChromeVer = (empty($_GET['ChromeVer']))?"":$_GET['ChromeVer'];
 
-exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/test.py", $ResultArr);
+if(empty($ChromeVer)){
+  exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/test.py", $ResultArr);
+} else {
+  exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/testver.py {$ChromeVer}", $ResultArr);
+}
 $PageHtml = implode("\n", $ResultArr);
 echo $PageHtml;
