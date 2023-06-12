295bc80fdcc6bbe0ec4037e9fbe222ebee24ff5c
jungmini <mins0428@gmail.com>
2023-06-12 12:53:47 +0900

    Create Item.py

---
+++ _DhGateSc_/Item.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*- 
+# 카테고리 파일-v4
+
+import time
+import sys
+import json
+import io
+import os
+import requests
+import traceback
+
+#한글깨짐
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+
+try :
+	os.system("killall -o 3m chrome")
+	os.system("killall -o 3m chromedriver")
+except :
+	pass
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+#pip3 install webdriver_manager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+import undetected_chromedriver as uc
+
+from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse, unquote
+
+CslId_SiteUrl = []
+
+
+MConfigData = sys.argv[1]
+MConfig = json.loads(MConfigData)
+
+CslId_SiteUrl = MConfig['CslId_SiteUrl']
+CustId = MConfig['CustId']
+Scroll = MConfig['Scroll']
+
+FileSendSave = MConfig['FileSendSave']
+NtosServer = MConfig['NtosServer']
+UserAgent = MConfig['UserAgent']
+CookiesLang = MConfig['CookiesLang']	#en / ko
+FileSaveDir = MConfig['FileSaveDir']
+ChromeVer = MConfig['ChromeVer']
+
+def osgzip(File) :
+	os.system("gzip "+File)
+
+def chromeWebdriver():
+	chrome_service = ChromeService(ChromeDriverManager().install())
+	chrome_options = uc.ChromeOptions()
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+	chrome_options.add_argument('--start-maximized')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+	chrome_options.add_argument('--disable-infobars')
+	chrome_options.add_argument('--ignore-certificate-errors')
+	chrome_options.add_argument('--ignore-ssl-errors=yes')
+	chrome_options.add_argument('--disable-gpu')
+	if ChromeVer :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=ChromeVer)
+	else :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
+	return driver
+
+driver = chromeWebdriver()
+
+driver.get("https://www.dhgate.com")
+getcookies = driver.get_cookies()
+driver.delete_all_cookies()
+
+for cookie in getcookies :
+	arr = {}
+	if cookie['name'] == "b2b_ship_country" :
+		cookie['value'] = "KR"
+	if cookie['name'] == "b2b_ip_country" :
+		cookie['value'] = "US"
+
+	for val in cookie.keys() :
+		arr[val] = cookie[val]
+	driver.add_cookie(arr)
+  
+driver.maximize_window()
+
+for val in IslId_SiteUrl :
+	(SiteUrl, SaveFileName) = val.split("|@|")
+	OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>"
+	#저장파일명
+	SaveFile = FileSaveDir+str(SaveFileName)
+	SaveFile = SaveFile.replace('.html', '_'+str(time.strftime('%H%M', time.localtime(time.time())))+'.html')
+	#에러msg
+	ErrMsg = ''
+	if SiteUrl == "" or SaveFileName == "" :
+		continue
+	else :
+		try :
+			driver.get(SiteUrl)
+			driver.implicitly_wait(3)
+			#driver.execute_script("window.stop();")
+			PageHtml = driver.page_source
+			NowUrl = driver.current_url
+		except :
+			PageHtml = ""
+			NowUrl = ""
+			ErrMsg = traceback.format_exc()
+		WriteFile = "<time>"+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"</time>\n"
+		WriteFile = WriteFile + OriginUrl+"\n"
+		if NowUrl :
+			WriteFile = WriteFile + "<ntosnowurl>"+NowUrl+"</ntosnowurl>\n"
+
+		if ErrMsg :
+			WriteFile = WriteFile + "<ErrMsg>"+str(ErrMsg)+"</ErrMsg>\n"
+			
+		#lock 체크
+		if PageHtml != "" :
+			lock_chk = ""
+			lock_chk = LockChk(PageHtml)
+			if lock_chk != "" :
+				WriteFile = WriteFile + "<lock_chk>"+lock_chk+"</lock_chk>\n"
+
+			driver.implicitly_wait(10)
+			PageHtml = driver.page_source
+			NowUrl = driver.current_url
+			
+		WriteFile = WriteFile + PageHtml
+
+		f = open(SaveFile, 'w', encoding="utf8")
+		f.write(WriteFile)
+		f.close()
+		osgzip(SaveFile)
+		if FileSendSave == "Y" and NtosServer != "" :
+			gzfile = SaveFile+".gz"
+			files = open(gzfile, 'rb')
+			upload = {'file': files}
+			data = {'CustId':CustId, 'ScrapType':'item' }
+			Result_ = requests.post(NtosServer, data=data, files=upload)
+			Result = Result_.text
+			if os.path.exists(gzfile) :
+				os.remove(gzfile)
+driver.quit()
