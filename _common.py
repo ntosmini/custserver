01743c919e579879147e71343fb9ec08540a3d30
jungmini <mins0428@gmail.com>
2023-06-15 08:34:15 +0900

    Add files via upload

---
+++ _common.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*- 
+import os
+import sys
+import time
+import json
+import io
+import requests
+import traceback
+import random
+import re
+
+from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse, unquote
+
+
+#>>>>>>>>>>>>>>>>>>>>>
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+#pip3 install webdriver_manager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+import undetected_chromedriver as uc
+#<<<<<<<<<<<<<<<<<<<<<
+
+ChromeVer = "114"
+
+def chromeWebdriver():
+	chrome_service = ChromeService(ChromeDriverManager().install())
+	chrome_options = uc.ChromeOptions()
+	chrome_options.add_argument('--headless=new')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+	chrome_options.add_argument('--start-maximized')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+	chrome_options.add_argument('--disable-infobars')
+	chrome_options.add_argument('--ignore-certificate-errors')
+	chrome_options.add_argument('--ignore-ssl-errors=yes')
+	chrome_options.add_argument('--disable-gpu')
+	chrome_options.page_load_strategy = 'normal'
+	if ChromeVer :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=ChromeVer)
+	else :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
+	return driver
