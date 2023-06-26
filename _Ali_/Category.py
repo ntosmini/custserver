67b6e2843706dbad0474d05b2f63ad4e30390971
jungmini <mins0428@gmail.com>
2023-06-26 11:42:45 +0900

    Update Category.py

--- _Ali_/Category.py
+++ _Ali_/Category.py
@@ -149,7 +149,7 @@ def LockChk(PageHtml) :
 
 	PageHtml2 = driver.page_source
 
-	if ResultChk != "pass" :
+	if ResultChk != "pass" and LockChkCnt < 5 :
 		LockChk(PageHtml2)
 
 	return str(ResultChk)+" - "+str(PageChk)+" - "+str(LockChkCnt)
