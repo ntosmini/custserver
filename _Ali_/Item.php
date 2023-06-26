7ca384b2091a0047c19f2c20a2764acba6007f96
jungmini <mins0428@gmail.com>
2023-06-26 09:47:34 +0900

    Add files via upload

---
+++ _Ali_/Item.php
@@ -0,0 +1,32 @@
+<?php
+set_time_limit(0);
+header("Content-Type: text/html; charset=UTF-8");
+/*
+상품 - 스트레이트
+*/
+
+$RunData = array();
+$RunData['IslId_SiteUrl'] = (empty($_POST['IslId_SiteUrl']))?"":$_POST['IslId_SiteUrl'];
+$RunData['CustId'] = (empty($_POST['CustId']))?"":$_POST['CustId'];
+
+$RunData['FileSendSave'] = (empty($_POST['FileSendSave']))?"N":$_POST['FileSendSave'];	//파일 저장 전송 사용여부 Y/N
+$RunData['NtosServer'] = (empty($_POST['NtosServer']))?"":$_POST['NtosServer'];	//받을 url
+$RunData['UserAgent'] = (empty($_POST['UserAgent']))?"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36":$_POST['UserAgent'];
+$RunData['ChromeVer'] = (empty($_POST['ChromeVer']))?"":$_POST['ChromeVer'];	//Chrome version
+
+
+if(empty($RunData['IslId_SiteUrl'])){
+	echo 'not IslId_SiteUrl';
+	exit;
+}
+$RunData['IslId_SiteUrl'] = explode("|^|", $RunData['IslId_SiteUrl']);
+
+if(empty($RunData['CustId'])){
+	echo 'not CustId';
+	exit;
+}
+
+$MConfigData = escapeshellarg(json_encode($RunData));
+
+
+exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Ali_/Item.py {$MConfigData}");
---
+++ _Ali_/ItemM.selenium.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*- 
+#알리 상품 모바일 selenium
