275dc532d449873d483474a435d732808b9336b5
jungmini <mins0428@gmail.com>
2023-07-21 11:01:42 +0900

    Update and rename ServerChk.v4.py to ServerChk.py

--- _NtosSc_/ServerChk.py
+++ _NtosSc_/ServerChk.py
@@ -7,6 +7,7 @@ import traceback
 MConfigData = sys.argv[1]
 MConfig = json.loads(MConfigData)
 SiteUrl = MConfig['SiteUrl']
+ChromeVer = MConfig['ChromeVer']
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -18,21 +19,25 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-executable_path = ChromeDriverManager().install()
+import undetected_chromedriver as uc
+
 def chromeWebdriver():
-	chrome_service = ChromeService(executable_path)
-	chrome_options = Options()
-	chrome_options.add_experimental_option('detach', True)
-	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+	chrome_service = ChromeService(executable_path = ChromeDriverManager().install())
+	chrome_options = uc.ChromeOptions()
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-	chrome_options.add_argument('window-size=1920,1080')
+	chrome_options.add_argument('--start-maximized')
 	chrome_options.add_argument('--disable-dev-shm-usage')
 	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 	chrome_options.add_argument('--disable-infobars')
-	chrome_options.page_load_strategy = 'normal'
-	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+	chrome_options.add_argument('--ignore-certificate-errors')
+	chrome_options.add_argument('--ignore-ssl-errors=yes')
+	chrome_options.add_argument('--disable-gpu')
+	if ChromeVer :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=ChromeVer)
+	else :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
 	return driver
 
 
