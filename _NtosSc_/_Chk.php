402d6bde04e8ecd877848792ed13cf8c82692eb8
jungmini <mins0428@gmail.com>
2023-07-21 15:17:01 +0900

    Update _Chk.php

--- _NtosSc_/_Chk.php
+++ _NtosSc_/_Chk.php
@@ -1,7 +1,9 @@
 <?php
 set_time_limit(0);
 header("Content-Type: text/html; charset=UTF-8");
-
+/*
+체크용 지우면 안됨
+*/
 exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_NtosSc_/_Chk.py", $ResultArr);
 $PageHtml = implode("\n", $ResultArr);
 
