c31499c167c34640d6c1290c3f89611b56d86265
jungmini <mins0428@gmail.com>
2023-07-24 09:30:42 +0900

    Update ServerChk.php

--- _NtosSc_/ServerChk.php
+++ _NtosSc_/ServerChk.php
@@ -38,6 +38,7 @@ if(empty($SId)){
 }
 
 if($Type == "web"){
+	$MiniServerGet['Type'] = $Type;
 	$MiniServerGet['CustId'] = $CustId;
 	$MiniServerGet['SId'] = $SId;
 	$MiniServerGet['PageHtml'] = "ntoswebsuccess";
