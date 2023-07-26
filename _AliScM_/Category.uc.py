72daabdfd5ff6854a6d6b688edb0f86454a189d5
jungmini <mins0428@gmail.com>
2023-07-26 16:15:52 +0900

    Update Category.uc.py

--- _AliScM_/Category.uc.py
+++ _AliScM_/Category.uc.py
@@ -1,2 +1,105 @@
 # -*- coding: utf-8 -*- 
 #알리 카테고리 모바일 uc
+import time
+import sys
+import json
+import io
+import os
+import random
+import requests
+import traceback
+import re
+
+#한글깨짐
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+IslId_SiteUrl = []
+
+MConfigData = sys.argv[1]
+MConfig = json.loads(MConfigData)
+
+IslId_SiteUrl = MConfig['IslId_SiteUrl']
+CustId = MConfig['CustId']
+
+FileSendSave = MConfig['FileSendSave']  #저장후 전송여부
+NtosServer = MConfig['NtosServer']  #전송서버 url
+
+FileDir = MConfig['FileDir']  #저장폴더
+LangType = MConfig['LangType']	#언어 및 통화 (ko | en)
+
+Scroll = MConfig['Scroll']  #스크롤
+StartUrl = MConfig['StartUrl']  #시작URL
+ChromeVer = MConfig['ChromeVer']  #크롬버전
+
+def chromeWebdriver():
+	chrome_service = ChromeService(ChromeDriverManager().install())
+	chrome_options = uc.ChromeOptions()
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+	chrome_options.add_argument('--start-maximized')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+	chrome_options.add_argument('--disable-infobars')
+	chrome_options.add_argument('--ignore-certificate-errors')
+	chrome_options.add_argument('--ignore-ssl-errors=yes')
+	chrome_options.add_argument('--disable-gpu')
+	if ChromeVer :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=ChromeVer)
+	else :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
+	return driver
+
+driver = chromeWebdriver()
+
+if StartUrl :
+	driver.get(StartUrl)
+	driver.implicitly_wait(10)
+
+for val in SiteUrlList :
+	#에러msg
+	ErrMsg = ''
+	PageHtml = ''
+	NowUrl = ''
+	(SiteUrl, SaveFileName) = val.split("|@|")
+	OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n\n"
+	#저장파일명
+	SaveFile = FileDir+str(SaveFileName)
+	SaveFile = SaveFile.replace('.html', '_'+str(time.strftime('%H%M', time.localtime(time.time())))+'.html')
+	if SiteUrl == "" or SaveFileName == "" :
+		pass
+	else :
+		try :
+			driver.get(SiteUrl)
+			PageHtml = driver.page_source
+			NowUrl = driver.current_url
+		except :
+			PageHtml = ''
+			PageHtmlRecode = 'error'
+			ErrMsg = str(traceback.format_exc())
+
+		if NowUrl :
+			NowUrl = "<ntosnowurl>"+NowUrl+"</ntosnowurl>\n\n"
+
+		if ErrMsg :
+			ErrMsg = "<error>"+ErrMsg+"</error>\n\n"
+
+		WriteFile = "<time>"+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"</time>\n\n"
+		WriteFile = WriteFile + OriginUrl + NowUrl + PageHtml + ErrMsg
+
+		f = open(SaveFile, 'w', encoding="utf8")
+		f.write(WriteFile)
+		f.close()
+		os.system("gzip "+SaveFile)
+
+		if FileSendSave == "Y" and NtosServer != "" :
+			gzfile = SaveFile+".gz"
+			files = open(gzfile, 'rb')
+			upload = {'file': files}
+			data = {'CustId':CustId, 'ScrapType':'cate' }
+			Result_ = requests.post(NtosServer, data=data, files=upload)
+			Result = Result_.text
+			if os.path.exists(gzfile) :
+				os.remove(gzfile)
+	time.sleep(random.randint(3, 6))
