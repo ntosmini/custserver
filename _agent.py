704a0aa957120a7bba8719a068c5c467fa63bbd3
jungmini <mins0428@gmail.com>
2023-07-26 10:02:10 +0900

    Update _agent.py

--- _agent.py
+++ _agent.py
@@ -959,3 +959,9 @@ def get_mobile_agent():
 	]
 
 	return list[random.randint(0,len(list)-1)]
+
+def get_pc_agent():
+	list = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+		,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+	       ]
+	return list[random.randint(0,len(list)-1)]
