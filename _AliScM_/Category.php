bdb3581c0202ec67a7e17d344ff2d7416495b7ca
jungmini <mins0428@gmail.com>
2023-07-26 11:14:32 +0900

    Create Category.php

---
+++ _AliScM_/Category.php
@@ -0,0 +1,49 @@
+<?php
+set_time_limit(0);
+header("Content-Type: text/html; charset=UTF-8");
+/*
+상품 - 모바일
+*/
+
+$RunData = array();
+$ScrapType =(empty($_POST['ScrapType']))?"":$_POST['ScrapType'];  // curl / selenium / uc
+$RunData['CustId'] = (empty($_POST['CustId']))?"":$_POST['CustId'];
+$RunData['SiteUrlList'] = (empty($_POST['SiteUrlList']))?"":$_POST['SiteUrlList'];
+$RunData['FileSendSave'] = (empty($_POST['FileSendSave']))?"N":$_POST['FileSendSave'];	//파일 저장 전송 사용여부 Y/N
+$RunData['NtosServer'] = (empty($_POST['NtosServer']))?"":$_POST['NtosServer'];	//받을 url
+$RunData['FileDir']  =(empty($_POST['FileDir']))?"":$_POST['FileDir'];  //저장폴더
+$RunData['LangType']  =(empty($_POST['LangType']))?"":$_POST['LangType'];  //언어 및 통화 (ko | en)
+
+
+
+if(empty($RunData['SiteUrlList'])){
+	echo 'not SiteUrlList';
+	exit;
+}
+$RunData['SiteUrlList'] = explode("|^|", $RunData['SiteUrlList']);
+
+if(empty($RunData['CustId'])){
+	echo 'not CustId';
+	exit;
+}
+if(empty($RunData['FileDir'])){
+	echo 'not FileDir';
+	exit;
+}
+$MConfigData = escapeshellarg(json_encode($RunData));
+
+switch($ScrapType){
+	case 'curl' :
+		exec("python3 /home/ntosmini/public_html/_AliScM_/Category.curl.py {$MConfigData}", $ResultArr);
+	break;
+	case 'uc' :
+		exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_AliScM_/Category.uc.py {$MConfigData}", $ResultArr);
+	break;
+	default :
+		exec("python3 /home/ntosmini/public_html/_AliScM_/Category.selenium.py {$MConfigData}", $ResultArr);
+	break;
+}
+if(isset($ResultArr) && count($ResultArr) > 0){
+	$PageHtml = implode("\n", $ResultArr);
+	echo $PageHtml;
+}
