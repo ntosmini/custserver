cb1f047cb7416302fa72144b28d01e1ad4b07622
jungmini <mins0428@gmail.com>
2023-08-16 08:54:11 +0900

    Update Item.curl.py

--- _AliScM_/Item.curl.py
+++ _AliScM_/Item.curl.py
@@ -48,97 +48,100 @@ UserAgent = _agent.get_mobile_agent()
 headers = {
 	"User-Agent":UserAgent
 }
-
-for val in IslId_SiteUrl :
-	#저장html
-	SaveHtml = ''
-	#에러msg
-	ErrMsg = ''
-	
-	PageHtml = ''
-	PageHtmlJson = ''
-	DetailUrl = ''
-	DetailHtml = ''
-	
-	(SiteUrl, SaveFileName) = val.split("|@|")
-	OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n\n"
-	OriginUrl = OriginUrl + len(IslId_SiteUrl)+"\n\n"
-	#저장파일명
-	SaveFile = FileDir+str(SaveFileName)
-	SaveFile = SaveFile.replace('.html', '_'+str(time.strftime('%H%M', time.localtime(time.time())))+'.html')
-  
-	if SiteUrl == "" or SaveFileName == "" :
-		continue
-  
-	try :
-		if LangType == "ko" :
-			PageHtml = requests.get(SiteUrl, headers=headers, cookies=cookies_ko)
-		elif LangType == "en" :
-			PageHtml = requests.get(SiteUrl, headers=headers, cookies=cookies_en)
-		else :
-			PageHtml = requests.get(SiteUrl, headers=headers)
-		PageHtmlRecode = PageHtml.status_code
-		PageHtml = PageHtml.text
-	except :
+try : 
+	for val in IslId_SiteUrl :
+		#저장html
+		SaveHtml = ''
+		#에러msg
+		ErrMsg = ''
+		
 		PageHtml = ''
-		PageHtmlRecode = 'error'
-		ErrMsg = ErrMsg + str(traceback.format_exc()) + "\n\n"
-
-	if PageHtml :
+		PageHtmlJson = ''
+		DetailUrl = ''
+		DetailHtml = ''
+		
+		(SiteUrl, SaveFileName) = val.split("|@|")
+		OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n\n"
+		OriginUrl = OriginUrl + len(IslId_SiteUrl)+"\n\n"
+		#저장파일명
+		SaveFile = FileDir+str(SaveFileName)
+		SaveFile = SaveFile.replace('.html', '_'+str(time.strftime('%H%M', time.localtime(time.time())))+'.html')
+	  
+		if SiteUrl == "" or SaveFileName == "" :
+			continue
+	  
 		try :
-			PageHtml = re.sub('\n', '', PageHtml)
-			PageHtmlJsonSearch = re.search(r'window.runParams\s+=\s+{\s+ data:(?P<JsonData>.*)};\s+</script>', str(PageHtml), re.DOTALL)
-			PageHtmlJsonData = PageHtmlJsonSearch.group('JsonData')
-			PageHtmlJson = json.loads(PageHtmlJsonData)
+			if LangType == "ko" :
+				PageHtml = requests.get(SiteUrl, headers=headers, cookies=cookies_ko)
+			elif LangType == "en" :
+				PageHtml = requests.get(SiteUrl, headers=headers, cookies=cookies_en)
+			else :
+				PageHtml = requests.get(SiteUrl, headers=headers)
+			PageHtmlRecode = PageHtml.status_code
+			PageHtml = PageHtml.text
 		except :
-			PageHtmlJson = ''
+			PageHtml = ''
+			PageHtmlRecode = 'error'
 			ErrMsg = ErrMsg + str(traceback.format_exc()) + "\n\n"
 
-	if PageHtmlJson :
-		SaveHtml = SaveHtml + "<PageHtmlRecode>" + str(PageHtmlRecode) + "</PageHtmlRecode>\n\n"
-		SaveHtml = SaveHtml + "<PageHtmlJson>" + str(PageHtmlJsonData) + "</PageHtmlJson>\n\n"
-		try :
-			DetailUrl = PageHtmlJson['descInfo']['productDescUrl']
-		except :
-			DetailUrl = '';
-			ErrMsg = ErrMsg + str(traceback.format_exc()) + "\n\n"
-
-		if DetailUrl :
+		if PageHtml :
 			try :
-				if LangType == "ko" :
-					DetailHtml = requests.get(str(DetailUrl), headers=headers, cookies=cookies_ko)
-				elif LangType == "en" :
-					DetailHtml = requests.get(str(DetailUrl), headers=headers, cookies=cookies_en)
-				else :
-					DetailHtml = requests.get(str(DetailUrl), headers=headers)
-				DetailHtmlRecode = DetailHtml.status_code
-				DetailHtml = DetailHtml.text
+				PageHtml = re.sub('\n', '', PageHtml)
+				PageHtmlJsonSearch = re.search(r'window.runParams\s+=\s+{\s+ data:(?P<JsonData>.*)};\s+</script>', str(PageHtml), re.DOTALL)
+				PageHtmlJsonData = PageHtmlJsonSearch.group('JsonData')
+				PageHtmlJson = json.loads(PageHtmlJsonData)
 			except :
-				DetailHtml = ''
-				DetailHtmlRecode = 'error'
+				PageHtmlJson = ''
 				ErrMsg = ErrMsg + str(traceback.format_exc()) + "\n\n"
 
-		if DetailHtml :
-			DetailHtml = re.sub('(<script[^<]+</script>)', '', DetailHtml)
-			DetailHtml = re.sub('(<a[^<]+</a>)', '', DetailHtml)
-			DetailHtml = re.sub('(<link[^>]+>)', '', DetailHtml)
-			SaveHtml = SaveHtml + "<DetailHtml>" + str(DetailHtml) + "</DetailHtml>\n\n"
+		if PageHtmlJson :
+			SaveHtml = SaveHtml + "<PageHtmlRecode>" + str(PageHtmlRecode) + "</PageHtmlRecode>\n\n"
+			SaveHtml = SaveHtml + "<PageHtmlJson>" + str(PageHtmlJsonData) + "</PageHtmlJson>\n\n"
+			try :
+				DetailUrl = PageHtmlJson['descInfo']['productDescUrl']
+			except :
+				DetailUrl = '';
+				ErrMsg = ErrMsg + str(traceback.format_exc()) + "\n\n"
 
-		WriteFile = "<time>"+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"</time>\n\n"
-		WriteFile = WriteFile + OriginUrl + SaveHtml + ErrMsg
+			if DetailUrl :
+				try :
+					if LangType == "ko" :
+						DetailHtml = requests.get(str(DetailUrl), headers=headers, cookies=cookies_ko)
+					elif LangType == "en" :
+						DetailHtml = requests.get(str(DetailUrl), headers=headers, cookies=cookies_en)
+					else :
+						DetailHtml = requests.get(str(DetailUrl), headers=headers)
+					DetailHtmlRecode = DetailHtml.status_code
+					DetailHtml = DetailHtml.text
+				except :
+					DetailHtml = ''
+					DetailHtmlRecode = 'error'
+					ErrMsg = ErrMsg + str(traceback.format_exc()) + "\n\n"
 
-		f = open(SaveFile, 'w', encoding="utf8")
-		f.write(WriteFile)
-		f.close()
-		os.system("gzip "+SaveFile)
+			if DetailHtml :
+				DetailHtml = re.sub('(<script[^<]+</script>)', '', DetailHtml)
+				DetailHtml = re.sub('(<a[^<]+</a>)', '', DetailHtml)
+				DetailHtml = re.sub('(<link[^>]+>)', '', DetailHtml)
+				SaveHtml = SaveHtml + "<DetailHtml>" + str(DetailHtml) + "</DetailHtml>\n\n"
 
-		if FileSendSave == "Y" and NtosServer != "" :
-			gzfile = SaveFile+".gz"
-			files = open(gzfile, 'rb')
-			upload = {'file': files}
-			data = {'CustId':CustId, 'ScrapType':'item' }
-			Result_ = requests.post(NtosServer, data=data, files=upload)
-			Result = Result_.text
-			if os.path.exists(gzfile) :
-				os.remove(gzfile)
-	time.sleep(random.randint(1, 3))
+			WriteFile = "<time>"+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"</time>\n\n"
+			WriteFile = WriteFile + OriginUrl + SaveHtml + ErrMsg
+
+			f = open(SaveFile, 'w', encoding="utf8")
+			f.write(WriteFile)
+			f.close()
+			os.system("gzip "+SaveFile)
+
+			if FileSendSave == "Y" and NtosServer != "" :
+				gzfile = SaveFile+".gz"
+				files = open(gzfile, 'rb')
+				upload = {'file': files}
+				data = {'CustId':CustId, 'ScrapType':'item' }
+				Result_ = requests.post(NtosServer, data=data, files=upload)
+				Result = Result_.text
+				if os.path.exists(gzfile) :
+					os.remove(gzfile)
+		time.sleep(random.randint(1, 3))
+except :
+	ErrMsg = ErrMsg + str(traceback.format_exc()) + "\n\n"
+	print(ErrMsg)
