e436152fe1ea6816833579ec60ba73f7eabe33c0
jungmini <mins0428@gmail.com>
2023-08-17 17:04:13 +0900

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -1,18 +1,37 @@
 # -*- coding: utf-8 -*- 
 
-from selenium import webdriver 
-
-import chromedriver_autoinstaller
+import time
+import sys
+import codecs
+import random
+#pip install beautifulsoup4     <=> pip install bs4
+import threading
+import json
+import re
+import io
+import os
+import requests
 import traceback
-PageHtml = 'no'
-try :
-	chromedriver_autoinstaller.install()
-	driver = webdriver.Chrome()
-	driver.get("http://ntos.co.kr")
-	driver.implicitly_wait(10)
-	PageHtml = driver.page_source
-except :
-	print(str(traceback.format_exc()))
-print(PageHtml)
-driver.close()
-driver.quit()
+import datetime
+import shutil
+
+from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+#pip3 install webdriver_manager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+#한글깨짐
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+path_ = ChromeDriverManager().install()
+print(path_)
