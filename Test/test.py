8f6e8ffe892d85911d745bbb36ddd25822d5ac90
jungmini <mins0428@gmail.com>
2023-08-17 16:38:52 +0900

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -1,16 +1,18 @@
+# -*- coding: utf-8 -*- 
+
 from selenium import webdriver as wd
 import chromedriver_autoinstaller
 import time
 import traceback
-PageHtml = ''
+PageHtml = 'no'
 try :
-  path = chromedriver_autoinstaller.install()
-  driver = wd.Chrome(path)
-  driver.get("http://ntos.co.kr")
-  driver.implicitly_wait(10)
-  PageHtml = driver.page_source
+	path = chromedriver_autoinstaller.install()
+	driver = wd.Chrome(path)
+	driver.get("http://ntos.co.kr")
+	driver.implicitly_wait(10)
+	PageHtml = driver.page_source
 except :
-  print(str(traceback.format_exc()))
+	print(str(traceback.format_exc()))
 print(PageHtml)
 driver.close()
 driver.quit()
