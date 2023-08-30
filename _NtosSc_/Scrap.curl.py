277e9038a3c55ba3499303693c078ba2b7d2bf88
jungmini <mins0428@gmail.com>
2023-08-30 15:38:58 +0900

    Update Scrap.curl.py

--- _NtosSc_/Scrap.curl.py
+++ _NtosSc_/Scrap.curl.py
@@ -34,56 +34,57 @@ NtosServer = MConfig['NtosServer']	#받을 url
 
 UserAgent = MConfig['UserAgent']
 ChromeVer = MConfig['ChromeVer']
-try :
-	headers = ""
-	if UserAgent :
-		headers = {
-			"User-Agent":UserAgent
-		}
-	for val in SiteUrlArr :
-		(CustId, SiteUrl, SaveFileName) = val.split("|@|")
-		OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n"
 
-		PageHtml = ""
-		NowUrl = ""
-		if SiteUrl == "" or SaveFileName == "" :
-			pass
-		else :
-			try :
-				if headers :
-					PageHtml = requests.get(SiteUrl, headers=headers)
-				else :
-					PageHtml = requests.get(SiteUrl)
-				PageHtml = PageHtml.text
-			except :
-				PageHtml = str(traceback.format_exc())
-			
-			WriteFile = "<time>"+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"</time>\n\n"
-			WriteFile = WriteFile + str(OriginUrl)
-			if NowUrl :
-				WriteFile = WriteFile + "<ntosnowurl>"+NowUrl+"</ntosnowurl>\n"
-			
-			WriteFile = WriteFile + PageHtml
+headers = ""
+if UserAgent :
+	headers = {
+		"User-Agent":UserAgent
+	}
+for val in SiteUrlArr :
+	(CustId, SiteUrl, SaveFileName) = val.split("|@|")
+	OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n"
 
-			if FileSaveDir :
-				SaveFile = FileSaveDir+str(SaveFileName)
-				SaveFile = SaveFile.replace('.html', '_'+str(time.strftime('%H%M', time.localtime(time.time())))+'.html')
-				f = open(SaveFile, 'w', encoding="utf8")
-				f.write(WriteFile)
-				f.close()
-				os.system("gzip "+SaveFile)
-
-				if FileSendSave == "y" and NtosServer != "" :
-					gzfile = SaveFile+".gz"
-					files = open(gzfile, 'rb')
-					upload = {'file': files}
-					data = {'CustId':CustId, 'ScrapType':'cate' }
-					Result_ = requests.post(NtosServer, data=data, files=upload)
-					Result = Result_.text
-					if os.path.exists(gzfile) :
-						os.remove(gzfile)
+	PageHtml = ""
+	NowUrl = ""
+	if SiteUrl == "" or SaveFileName == "" :
+		pass
+	else :
+		try :
+			if headers :
+				PageHtml = requests.get(SiteUrl, headers=headers)
 			else :
+				PageHtml = requests.get(SiteUrl)
+			PageHtml = PageHtml.text
+		except :
+			PageHtml = str(traceback.format_exc())
+		
+		WriteFile = "<time>"+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"</time>\n\n"
+		WriteFile = WriteFile + str(OriginUrl)
+		if NowUrl :
+			WriteFile = WriteFile + "<ntosnowurl>"+NowUrl+"</ntosnowurl>\n"
+		
+		WriteFile = WriteFile + PageHtml
+
+		if FileSaveDir :
+			SaveFile = FileSaveDir+str(SaveFileName)
+			SaveFile = SaveFile.replace('.html', '_'+str(time.strftime('%H%M', time.localtime(time.time())))+'.html')
+			f = open(SaveFile, 'w', encoding="utf8")
+			f.write(WriteFile)
+			f.close()
+			os.system("gzip "+SaveFile)
+
+			if FileSendSave == "y" and NtosServer != "" :
+				gzfile = SaveFile+".gz"
+				files = open(gzfile, 'rb')
+				upload = {'file': files}
+				data = {'CustId':CustId }
+				Result_ = requests.post(NtosServer, data=data, files=upload)
+				Result = Result_.text
+				if os.path.exists(gzfile) :
+					os.remove(gzfile)
+		else :
+			if len(SiteUrlArr) == 1 :
 				print(str(WriteFile))
-		time.sleep(random.uniform(2, 4))
-except :
-	print(str(traceback.format_exc()))
+			else :
+				pass
+	time.sleep(random.uniform(2, 4))
