4fccf725c0c43a2975461b5877456369f8163b1d
jungmini <mins0428@gmail.com>
2023-08-30 12:40:36 +0900

    Update Scrap.uc.py

--- _NtosSc_/Scrap.uc.py
+++ _NtosSc_/Scrap.uc.py
@@ -65,53 +65,51 @@ def chromeWebdriver():
 	else :
 		driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
 	return driver
-try :
-	driver = chromeWebdriver()
 
-	for val in SiteUrlArr :
-		(CustId, SiteUrl, SaveFileName) = val.split("|@|")
-		OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n"
+driver = chromeWebdriver()
 
-		PageHtml = ""
-		NowUrl = ""
-		if SiteUrl == "" or SaveFileName == "" :
-			pass
+for val in SiteUrlArr :
+	(CustId, SiteUrl, SaveFileName) = val.split("|@|")
+	OriginUrl = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n"
+
+	PageHtml = ""
+	NowUrl = ""
+	if SiteUrl == "" or SaveFileName == "" :
+		pass
+	else :
+		try :
+			driver.get(SiteUrl)
+			driver.implicitly_wait(10)
+			PageHtml = driver.page_source
+			NowUrl = driver.current_url
+		except :
+			PageHtml = str(traceback.format_exc())
+		
+		WriteFile = "<time>"+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"</time>\n\n"
+		WriteFile = WriteFile + str(OriginUrl)
+		if NowUrl :
+			WriteFile = WriteFile + "<ntosnowurl>"+NowUrl+"</ntosnowurl>\n"
+		
+		WriteFile = WriteFile + PageHtml
+
+		if FileSaveDir :
+			SaveFile = FileSaveDir+str(SaveFileName)
+			SaveFile = SaveFile.replace('.html', '_'+str(time.strftime('%H%M', time.localtime(time.time())))+'.html')
+			f = open(SaveFile, 'w', encoding="utf8")
+			f.write(WriteFile)
+			f.close()
+			os.system("gzip "+SaveFile)
+
+			if FileSendSave == "y" and NtosServer != "" :
+				gzfile = SaveFile+".gz"
+				files = open(gzfile, 'rb')
+				upload = {'file': files}
+				data = {'CustId':CustId, 'ScrapType':'cate' }
+				Result_ = requests.post(NtosServer, data=data, files=upload)
+				Result = Result_.text
+				if os.path.exists(gzfile) :
+					os.remove(gzfile)
 		else :
-			try :
-				criver.get(SiteUrl)
-				driver.implicitly_wait(10)
-				PageHtml = driver.page_source
-				NowUrl = driver.current_url
-			except :
-				PageHtml = str(traceback.format_exc())
-			
-			WriteFile = "<time>"+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"</time>\n\n"
-			WriteFile = WriteFile + str(OriginUrl)
-			if NowUrl :
-				WriteFile = WriteFile + "<ntosnowurl>"+NowUrl+"</ntosnowurl>\n"
-			
-			WriteFile = WriteFile + PageHtml
-
-			if FileSaveDir :
-				SaveFile = FileSaveDir+str(SaveFileName)
-				SaveFile = SaveFile.replace('.html', '_'+str(time.strftime('%H%M', time.localtime(time.time())))+'.html')
-				f = open(SaveFile, 'w', encoding="utf8")
-				f.write(WriteFile)
-				f.close()
-				os.system("gzip "+SaveFile)
-
-				if FileSendSave == "y" and NtosServer != "" :
-					gzfile = SaveFile+".gz"
-					files = open(gzfile, 'rb')
-					upload = {'file': files}
-					data = {'CustId':CustId, 'ScrapType':'cate' }
-					Result_ = requests.post(NtosServer, data=data, files=upload)
-					Result = Result_.text
-					if os.path.exists(gzfile) :
-						os.remove(gzfile)
-			else :
-				print(str(WriteFile))
-		time.sleep(random.uniform(2, 4))
-	driver.quit()
-except :
-	print(str(traceback.format_exc()))
+			print(str(WriteFile))
+	time.sleep(random.uniform(2, 4))
+driver.quit()
