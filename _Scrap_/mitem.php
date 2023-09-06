d2954637148ba39f3298ee4aab890a58b8fbfcdf
jungmini <mins0428@gmail.com>
2023-09-06 12:56:10 +0900

    Update mitem.php

--- _Scrap_/mitem.php
+++ _Scrap_/mitem.php
@@ -2,14 +2,11 @@
 set_time_limit(0);
 header("Content-Type: text/html; charset=UTF-8");
 
-$RunData = array();
-$RunData['Type'] = (empty($_POST['Type']))?"server":$_POST['Type'];
-$RunData['Search'] = (empty($_POST['Search']))?"":$_POST['Search'];
-$RunData['SearchChk'] = (empty($_POST['SearchChk']))?"n":$_POST['SearchChk'];
+$Type = (empty($_POST['Type']))?"server":$_POST['Type'];
+$Search = (empty($_POST['Search']))?"n":$_POST['Search'];
+$SearchChk = (empty($_POST['SearchChk']))?"n":$_POST['SearchChk'];
 
-$MConfigData = escapeshellarg(json_encode($RunData));
-
-exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/mitem.py {$MConfigData}", $ResultArr);
+exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/mitem.py {$Type} {$Search} {$SearchChk}", $ResultArr);
 $PageHtml = implode("\n", $ResultArr);
 $PageHtml = strip_tags($PageHtml);
 echo $PageHtml;
