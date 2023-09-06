37743cf3e3f94942db3315bee41667aba8a567bf
jungmini <mins0428@gmail.com>
2023-09-07 07:13:55 +0900

    Update mitem.php

--- _Scrap_/mitem.php
+++ _Scrap_/mitem.php
@@ -3,10 +3,11 @@ set_time_limit(0);
 header("Content-Type: text/html; charset=UTF-8");
 
 $Type = (empty($_POST['Type']))?"server":$_POST['Type'];
+$StartUrl = (empty($_POST['StartUrl']))?"n":$_POST['StartUrl'];
 $Search = (empty($_POST['Search']))?"n":$_POST['Search'];
 $SearchChk = (empty($_POST['SearchChk']))?"n":$_POST['SearchChk'];
 
-exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/mitem.py {$Type} {$Search} {$SearchChk}", $ResultArr);
+exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/mitem.py {$Type} {$StartUrl} {$Search} {$SearchChk}", $ResultArr);
 $PageHtml = implode("\n", $ResultArr);
 $PageHtml = strip_tags($PageHtml);
 echo $PageHtml;
