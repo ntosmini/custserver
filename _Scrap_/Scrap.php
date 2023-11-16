61a663bd71a87ce09e63dd710ecad7343a9a007a
jungmini <mins0428@gmail.com>
2023-11-16 11:04:10 +0900

    Update Scrap.php

--- _Scrap_/Scrap.php
+++ _Scrap_/Scrap.php
@@ -12,9 +12,9 @@ if(empty($RunData['SiteUrl'])){
 }
 
 $MConfigData = escapeshellarg(json_encode($RunData));
-echo "ok-1";
+
 exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/Scrap.py {$MConfigData}", $ResultArr);
-echo "ok-2";
+
 $PageHtml = implode("\n", $ResultArr);
 echo $PageHtml;
 exit;
