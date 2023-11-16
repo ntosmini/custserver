ac0e4a1540ebd3d8f36ec6bd82936328756f05f2
jungmini <mins0428@gmail.com>
2023-11-16 10:59:30 +0900

    Update Scrap.py

--- _Scrap_/Scrap.py
+++ _Scrap_/Scrap.py
@@ -37,49 +37,50 @@ MConfig = json.loads(MConfigData)
 
 SiteUrl = MConfig['SiteUrl']
 SetAgent = MConfig['SetAgent']
-print(SiteUrl)
-exit()
-ChromeVer = "114"
-if SetAgent != "" :
-	sys.path.append(os.path.dirname("/home/ntosmini/public_html/_agent.py"))
-	import _agent
+try :
+	ChromeVer = "114"
+	if SetAgent != "" :
+		sys.path.append(os.path.dirname("/home/ntosmini/public_html/_agent.py"))
+		import _agent
+	
+		if SetAgent = "mobile" :
+			Agent = _agent.get_mobile_agent()
+		elif SetAgent = "pc" :
+			Agent = _agent.get_pc_agent()
+		else :
+			Agent = ""
+	
+	def chromeWebdriver():
+		chrome_service = ChromeService(ChromeDriverManager().install())
+		chrome_options = uc.ChromeOptions()
+		chrome_options.add_argument('--headless')
+		chrome_options.add_argument('--no-sandbox')
+		chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+		chrome_options.add_argument('--start-maximized')
+		chrome_options.add_argument('--disable-dev-shm-usage')
+		chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+		chrome_options.add_argument('--disable-infobars')
+		chrome_options.add_argument('--ignore-certificate-errors')
+		chrome_options.add_argument('--ignore-ssl-errors=yes')
+		chrome_options.add_argument('--disable-gpu')
+		if Agent != "" :
+			chrome_options.add_argument('--user-agent=' + Agent)
+		if ChromeVer :
+			driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=ChromeVer)
+		else :
+			driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
+		return driver
+	
+	driver = chromeWebdriver()
+	driver.get(SiteUrl)
+	driver.implicitly_wait(10)
+	
+	PageHtml = driver.page_source
+	NowUrl = driver.current_url
+	print(str(NowUrl))
+	print("\n\n")
+	print(str(PageHtml))
+	driver.quit()
+except :
+	print(str(traceback.format_exc()))
 
-	if SetAgent = "mobile" :
-		Agent = _agent.get_mobile_agent()
-	elif SetAgent = "pc" :
-		Agent = _agent.get_pc_agent()
-	else :
-		Agent = ""
-
-def chromeWebdriver():
-	chrome_service = ChromeService(ChromeDriverManager().install())
-	chrome_options = uc.ChromeOptions()
-	chrome_options.add_argument('--headless')
-	chrome_options.add_argument('--no-sandbox')
-	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-	chrome_options.add_argument('--start-maximized')
-	chrome_options.add_argument('--disable-dev-shm-usage')
-	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-	chrome_options.add_argument('--disable-infobars')
-	chrome_options.add_argument('--ignore-certificate-errors')
-	chrome_options.add_argument('--ignore-ssl-errors=yes')
-	chrome_options.add_argument('--disable-gpu')
-	if Agent != "" :
-		chrome_options.add_argument('--user-agent=' + Agent)
-	if ChromeVer :
-		driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=ChromeVer)
-	else :
-		driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
-	return driver
-
-driver = chromeWebdriver()
-driver.get(SiteUrl)
-driver.implicitly_wait(10)
-
-PageHtml = driver.page_source
-NowUrl = driver.current_url
-print(str(NowUrl))
-print("\n\n")
-print(str(PageHtml))
-
-driver.quit()
