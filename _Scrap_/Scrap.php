e013bca5a0cf8749e8351448cb8ad88332bc0941
jungmini <mins0428@gmail.com>
2023-11-16 10:32:04 +0900

    Add files via upload

--- _Scrap_/Scrap.php
+++ _Scrap_/Scrap.php
@@ -3,29 +3,18 @@ set_time_limit(0);
 header("Content-Type: text/html; charset=UTF-8");
 
 $RunData = array();
-$ScrapType = (empty($_POST['ScrapType']))?"uc":$_POST['ScrapType'];	//수집방식 - curl or self or selenium or pyget
 $RunData['SiteUrl'] = (empty($_POST['SiteUrl']))?"":$_POST['SiteUrl'];
-
-$RunData['CustId'] = (empty($_POST['CustId']))?"":$_POST['CustId'];
-$RunData['FileSendSave'] = (empty($_POST['FileSendSave']))?"n":$_POST['FileSendSave'];	//파일 저장 전송 사용여부 y/n
-$RunData['NtosServer'] = (empty($_POST['NtosServer']))?"":$_POST['NtosServer'];	//받을 url
-$RunData['FileDir'] = (empty($_POST['FileDir']))?"":$_POST['FileDir'];	//xs 서버 저장 폴더
+$RunData['SetAgent'] = (empty($_POST['SetAgent']))?"":$_POST['SetAgent'];
 
 if(empty($RunData['SiteUrl'])){
 	echo 'not SiteUrl';
 	exit;
 }
-$MConfigData = escapeshellarg(json_encode($RunData));
-switch($ScrapType){
-	case 'uc' :
-		exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/Scrap.uc.py {$MConfigData}", $ResultArr);
-	break;
 
-	default :
-		echo "ntoswebsuccess";
-		exit;
-	break;
-}
+$MConfigData = escapeshellarg(json_encode($RunData));
+
+exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/Scrap.py {$MConfigData}", $ResultArr);
+
 $PageHtml = implode("\n", $ResultArr);
 echo $PageHtml;
 exit;
--- _Scrap_/Scrap.py
+++ _Scrap_/Scrap.py
@@ -30,180 +30,55 @@ from selenium.webdriver.common.by import By
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
-SiteUrl_SaveFileName = []
+
 
 MConfigData = sys.argv[1]
 MConfig = json.loads(MConfigData)
 
-CustId = MConfig['CustId']
-SiteUrlOne = MConfig['SiteUrlOne']
-StartSiteUrl = MConfig['StartSiteUrl']
-SiteUrl_SaveFileName = MConfig['SiteUrl_SaveFileName']
-Refresh = MConfig['Refresh']
-Scroll = MConfig['Scroll']
-ScrapResultType = MConfig['ScrapResultType']
-FileSaveDir = MConfig['FileSaveDir']
-NtosSendServer = MConfig['NtosSendServer']
-UserAgent = MConfig['UserAgent']
+SiteUrl = MConfig['SiteUrl']
+SetAgent = MConfig['SetAgent']
 
-executable_path = ChromeDriverManager().install()
+ChromeVer = "114"
+if SetAgent != "" :
+	sys.path.append(os.path.dirname("/home/ntosmini/public_html/_agent.py"))
+	import _agent
+
+	if SetAgent = "mobile" :
+		Agent = _agent.get_mobile_agent()
+	elif SetAgent = "pc" :
+		Agent = _agent.get_pc_agent()
+	else :
+		Agent = ""
 
 def chromeWebdriver():
-	chrome_service = ChromeService(executable_path)
-	chrome_options = Options()
-	chrome_options.add_experimental_option('detach', True)
-	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+	chrome_service = ChromeService(ChromeDriverManager().install())
+	chrome_options = uc.ChromeOptions()
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-	chrome_options.add_argument('window-size=1920,1080')
+	chrome_options.add_argument('--start-maximized')
 	chrome_options.add_argument('--disable-dev-shm-usage')
 	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 	chrome_options.add_argument('--disable-infobars')
-	chrome_options.add_argument('user-agent=' + UserAgent)
-	chrome_options.page_load_strategy = 'normal'
-	driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+	chrome_options.add_argument('--ignore-certificate-errors')
+	chrome_options.add_argument('--ignore-ssl-errors=yes')
+	chrome_options.add_argument('--disable-gpu')
+	if Agent != "" :
+		chrome_options.add_argument('--user-agent=' + Agent)
+	if ChromeVer :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=ChromeVer)
+	else :
+		driver = uc.Chrome(service=chrome_service, options=chrome_options, use_subprocess=True)
 	return driver
 
 driver = chromeWebdriver()
+driver.get(SiteUrl)
+driver.implicitly_wait(10)
 
-if StartSiteUrl :
-	driver.get(StartSiteUrl)
-	driver.implicitly_wait(10)
-
-if SiteUrlOne == "N" :
-	for val in SiteUrl_SaveFileName :
-		PageHtml = ""
-		NowUrl = ""
-		SiteUrl = ""
-		SaveFileName = ""
-
-		try :
-			(SiteUrl, SaveFileName) = val.split("|@|")
-		except :
-			err = traceback.format_exc()
-			PageHtml = str(err)+"\n"
-		try :
-			driver.get(SiteUrl)
-			driver.implicitly_wait(10)
-			if Refresh == "Y" :
-				driver.refresh()
-				driver.implicitly_wait(10)
-
-			if Scroll == "Y" :
-				SCROLL_PAUSE_SEC = 0.5
-				# 스크롤 높이 가져옴
-				last_height = driver.execute_script("return document.body.scrollHeight")
-				while True:
-					# 끝까지 스크롤 다운
-					driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-					# SCROLL_PAUSE_SEC 초 대기
-					time.sleep(SCROLL_PAUSE_SEC)
-					# 스크롤 다운 후 스크롤 높이 다시 가져옴
-					new_height = driver.execute_script("return document.body.scrollHeight")
-					if new_height == last_height:
-						break
-					last_height = new_height
-
-			time.sleep(random.randint(1, 3))
-			PageHtml = driver.page_source
-			NowUrl = driver.current_url
-		except :
-			err = traceback.format_exc()
-			PageHtml = PageHtml+str(err)+"\n"
-
-		PageHtml = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n"+ "<ntosnowurl>"+str(NowUrl)+"</ntosnowurl>\n" + PageHtml
-
-		if ScrapResultType == "save" :
-			SaveFile = str(FileSaveDir)+str(SaveFileName)
-			WriteContent = PageHtml
-			f = open(SaveFile, 'w', encoding="utf8")
-			f.write(WriteContent)
-			f.close()
-			os.system("gzip "+SaveFile)
-		elif ScrapResultType == "send" :
-			SaveFile = str(FileSaveDir)+str(SaveFileName)
-			WriteContent = PageHtml
-			f = open(SaveFile, 'w', encoding="utf8")
-			f.write(WriteContent)
-			f.close()
-			os.system("gzip "+SaveFile)
-
-			gzfile = SaveFile+".gz"
-			files = open(gzfile, 'rb')
-			upload = {'file': files}
-			data = {'CustId':CustId }
-			Result_ = requests.post(NtosSendServer, data=data, files=upload)
-			res = Result_.text
-			time.sleep(3)
-			os.remove(gzfile)
-		else :
-			pass
-
-else :
-	PageHtml = ""
-	NowUrl = ""
-	SiteUrl = str(SiteUrlOne)
-	SaveFileName = ""
-
-	try :
-		driver.get(SiteUrl)
-		driver.implicitly_wait(10)
-
-		if Refresh == "Y" :
-			driver.refresh()
-			driver.implicitly_wait(10)
-
-		if Scroll == "Y" :
-			SCROLL_PAUSE_SEC = 0.5
-			# 스크롤 높이 가져옴
-			last_height = driver.execute_script("return document.body.scrollHeight")
-			while True:
-				# 끝까지 스크롤 다운
-				driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-				# SCROLL_PAUSE_SEC 초 대기
-				time.sleep(SCROLL_PAUSE_SEC)
-				# 스크롤 다운 후 스크롤 높이 다시 가져옴
-				new_height = driver.execute_script("return document.body.scrollHeight")
-				if new_height == last_height:
-					break
-				last_height = new_height
-
-		time.sleep(random.randint(1, 3))
-		PageHtml = driver.page_source
-		NowUrl = driver.current_url
-	except :
-		err = traceback.format_exc()
-		PageHtml = PageHtml+str(err)+"\n"
-
-	PageHtml = "<ntosoriginurl>"+str(SiteUrl)+"</ntosoriginurl>\n"+ "<ntosnowurl>"+str(NowUrl)+"</ntosnowurl>\n" + PageHtml
-
-	if ScrapResultType == "save" :
-		SaveFile = str(FileSaveDir)+str(SaveFileName)
-		WriteContent = PageHtml
-		f = open(SaveFile, 'w', encoding="utf8")
-		f.write(WriteContent)
-		f.close()
-		os.system("gzip "+SaveFile)
-	elif ScrapResultType == "send" :
-		SaveFile = str(FileSaveDir)+str(SaveFileName)
-		WriteContent = PageHtml
-		f = open(SaveFile, 'w', encoding="utf8")
-		f.write(WriteContent)
-		f.close()
-		os.system("gzip "+SaveFile)
-
-		gzfile = SaveFile+".gz"
-		files = open(gzfile, 'rb')
-		upload = {'file': files}
-		data = {'CustId':CustId }
-		Result_ = requests.post(NtosSendServer, data=data, files=upload)
-		res = Result_.text
-		time.sleep(3)
-		os.remove(gzfile)
-	elif ScrapResultType == "view" :
-		print(PageHtml)
-	else :
-		pass
+PageHtml = driver.page_source
+NowUrl = driver.current_url
+print(str(NowUrl))
+print("\n\n")
+print(str(PageHtml))
 
 driver.quit()
