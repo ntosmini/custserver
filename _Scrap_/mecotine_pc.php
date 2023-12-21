03249b75042990d89c59d454b9323f475b6cbefd
jungmini <mins0428@gmail.com>
2023-12-21 10:46:28 +0900

    Update mecotine_pc.php

--- _Scrap_/mecotine_pc.php
+++ _Scrap_/mecotine_pc.php
@@ -27,7 +27,7 @@ if(empty($RunData['SearchChk'])){
 
 $MConfigData = escapeshellarg(json_encode($RunData));
 
-exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/Scrap.py {$MConfigData}", $ResultArr);
+exec("PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin python3 /home/ntosmini/public_html/_Scrap_/mecotine_pc.py {$MConfigData}", $ResultArr);
 
 $PageHtml = implode("\n", $ResultArr);
 echo $PageHtml;
