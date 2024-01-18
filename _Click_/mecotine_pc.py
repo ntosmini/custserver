ca60e6cf4713ea1a5718c1ccdf00e0be7f078da0
jungmini <mins0428@gmail.com>
2024-01-19 08:50:39 +0900

    Update mecotine_pc.py

--- _Click_/mecotine_pc.py
+++ _Click_/mecotine_pc.py
@@ -202,7 +202,7 @@ try :
 
 		conloop = random.randint(1, 3)
 		for l in range(conloop) :
-			a_elements = driver.find_elements(By.CSS_SELECTOR, ".main_disp a[href*='shopdetail']")
+			a_elements = driver.find_elements(By.CSS_SELECTOR, "a[href*='shopdetail']")
 			a_emerand = random.randint(0, len(a_elements)-1)
 			a_emerand_target = a_elements[a_emerand]
 			TargetClick(a_emerand_target)
