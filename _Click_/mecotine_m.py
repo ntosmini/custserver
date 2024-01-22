e036c6e28c2f0f4226b0607761cd31184b7cf3e2
jungmini <mins0428@gmail.com>
2024-01-22 09:24:28 +0900

    Update mecotine_m.py

--- _Click_/mecotine_m.py
+++ _Click_/mecotine_m.py
@@ -219,7 +219,7 @@ try :
 	if MatchChk == "y" :
 		time.sleep(random.randint(3, 6))
 		ScrollDown(random.uniform(0.5, 1), 'num')
-		time.sleep(random.randint(10, 22))
+		time.sleep(random.randint(25, 35))
 
 		a_elements = driver.find_elements(By.CSS_SELECTOR, ".main_disp a[href*='shopdetail']")
 		a_emerand = random.randint(0, len(a_elements)-1)
